e0eda70d067a611fafd79b1ab939cc1615f791f9
Binit Shah <bshah@hello-robot.com>
2024-04-16 22:44:06 -0400

    Simplify send/recv StretchPy Protocol

--- src/stretch/comms/recv_protocol.py
+++ src/stretch/comms/recv_protocol.py
@@ -7,12 +7,10 @@ def initialize(ip_addr, port):
     ctx = zmq.Context()
     sock = ctx.socket(zmq.REQ)
     sock.connect(f"tcp://{ip_addr}:{port}")
-    poll = zmq.Poller()
-    poll.register(sock, flags=zmq.POLLIN)
-    return sock, poll
+    return sock
 
 
-def recv_spp(sock, poll):
+def recv_spp(sock):
     sock.send_string("requesting_stretchpy_protocol")
     server_protocol = sock.recv_string()
     if server_protocol != STRETCHPY_PROTOCOL:
