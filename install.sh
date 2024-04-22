1c2ab599e4730df8537c80c52565b63597021de4
Chris Paxton <cpaxton@hello-robot.com>
2024-04-22 19:28:16 -0400

    add installer

---
+++ install.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+# This script (c) 2024 Chris Paxton under the MIT license: https://opensource.org/licenses/MIT
+# This script is designed to install the HomeRobot/StretchPy environment.
+export PYTORCH_VERSION=2.1.2
+export CUDA_VERSION=11.8
+export PYTHON_VERSION=3.10
+ENV_NAME=stretchpy
+CUDA_VERSION_NODOT="${CUDA_VERSION//./}"
+echo "=============================================="
+echo "         INSTALLING STRETCH AI TOOLS"
+echo "=============================================="
+echo "Make sure:"
+echo " - Conda env is active"
+echo " - CUDA_HOME is set"
+echo "Currently:"
+echo " - CUDA_HOME=$CUDA_HOME"
+echo " - python=`which python`"
+echo ""
+echo "---------------------------------------------"
+echo "Environment name: $ENV_NAME"
+echo "PyTorch Version: $PYTORCH_VERSION"
+echo "CUDA Version: $CUDA_VERSION"
+echo "Python Version: $PYTHON_VERSION"
+echo "CUDA Version No Dot: $CUDA_VERSION_NODOT"
+echo "---------------------------------------------"
+read -p "Does all this look correct? (y/n) " yn
+case $yn in
+	y ) echo "Starting installation...";;
+	n ) echo "Exiting...";
+		exit;;
+	* ) echo Invalid response!;
+		exit 1;;
+esac
+conda env remove -n $ENV_NAME -y
+conda create -n $ENV_NAME -c pytorch -c nvidia pytorch=$PYTORCH_VERSION pytorch-cuda=$CUDA_VERSION python=$PYTHON_VERSION -y 
+source activate $ENV_NAME
+pip install torch_cluster -f https://pytorch-geometric.com/whl/torch-${PYTORCH_VERSION}+${CUDA_VERSION_NODOT}.html
+pip install torch_scatter -f https://pytorch-geometric.com/whl/torch-${PYTORCH_VERSION}+${CUDA_VERSION_NODOT}.html
+pip install torch_geometric
