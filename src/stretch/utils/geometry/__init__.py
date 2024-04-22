a253a5bffdc975814bbb457e1f42497b1db65ceb
Chris Paxton <cpaxton@hello-robot.com>
2024-04-22 16:53:51 -0400

    update

--- src/stretch/utils/geometry/__init__.py
+++ src/stretch/utils/geometry/__init__.py
@@ -3,6 +3,5 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
-
 from ._base import posquat2sophus, sophus2posquat, xyt2sophus, sophus2xyt, normalize_ang_error, xyt_global_to_base, xyt_base_to_global
 from .angles import PI2, angle_difference, interpolate_angles
