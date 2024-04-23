aaa82ede279b2dc3c3ef9c494fd56edf5cb6889b
Chris Paxton <165678659+hello-cpaxton@users.noreply.github.com>
2024-04-23 09:00:21 -0700

    motion checks, controllers, etc (#4)

--- src/setup.py
+++ src/setup.py
@@ -24,6 +24,7 @@ setuptools.setup(
         "opencv-python",
         "scipy",
         "matplotlib",
+        "sophuspy",
     ],
     extras_require={
         "dev": [
--- src/stretch/motion/utils/geometry.py
+++ src/stretch/motion/utils/geometry.py
@@ -3,10 +3,17 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
+from typing import Iterable, Tuple
 
 import numpy as np
+import sophuspy as sp
 
 PI2 = 2 * np.pi
+
+
+def normalize_ang_error(angle: float) -> float:
+    """Normalize an angle to the range [-pi, pi]."""
+    return (angle + np.pi) % PI2 - np.pi
 
 
 def angle_difference(angle1: float, angle2: float) -> float:
@@ -15,6 +22,66 @@ def angle_difference(angle1: float, angle2: float) -> float:
     angle2 = angle2 % PI2
     diff = np.abs(angle1 - angle2)
     return min(diff, PI2 - diff)
+
+
+def xyt_global_to_base(XYT, current_pose):
+    """
+    Transforms the point cloud into geocentric frame to account for
+    camera position
+    Input:
+        XYZ                     : ...x3
+        current_pose            : base position (x, y, theta (radians))
+    Output:
+        XYZ : ...x3
+    """
+    pose_world2target = xyt2sophus(XYT)
+    pose_world2base = xyt2sophus(current_pose)
+    pose_base2target = pose_world2base.inverse() * pose_world2target
+    return sophus2xyt(pose_base2target)
+
+
+def xyt_base_to_global(out_XYT, current_pose):
+    """
+    Transforms the point cloud from base frame into geocentric frame
+    Input:
+        XYZ                     : ...x3
+        current_pose            : base position (x, y, theta (radians))
+    Output:
+        XYZ : ...x3
+    """
+    pose_base2target = xyt2sophus(out_XYT)
+    pose_world2base = xyt2sophus(current_pose)
+    pose_world2target = pose_world2base * pose_base2target
+    return sophus2xyt(pose_world2target)
+
+
+def xyt2sophus(xyt: np.ndarray) -> sp.SE3:
+    """
+    Converts SE2 coordinates (x, y, rz) to an sophus SE3 pose object.
+    """
+    x = np.array([xyt[0], xyt[1], 0.0])
+    r_mat = sp.SO3.exp([0.0, 0.0, xyt[2]]).matrix()
+    return sp.SE3(r_mat, x)
+
+
+def sophus2xyt(se3: sp.SE3) -> np.ndarray:
+    """
+    Converts an sophus SE3 pose object to SE2 coordinates (x, y, rz).
+    """
+    x_vec = se3.translation()
+    r_vec = se3.so3().log()
+    return np.array([x_vec[0], x_vec[1], r_vec[2]])
+
+
+def posquat2sophus(pos: Iterable[float], quat: Iterable[float]) -> sp.SE3:
+    r_mat = Rotation.from_quat(quat).as_matrix()
+    return sp.SE3(r_mat, pos)
+
+
+def sophus2posquat(se3: sp.SE3) -> Tuple[Iterable[float], Iterable[float]]:
+    pos = se3.translation()
+    quat = Rotation.from_matrix(se3.so3().matrix()).as_quat()
+    return pos, quat
 
 
 def interpolate_angles(
