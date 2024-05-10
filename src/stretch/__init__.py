c82f74b16abbc23cbe36dd28c6ab9fa621d87290
Chris Paxton <cpaxton@hello-robot.com>
2024-05-10 11:49:10 -0400

    remove some prints that were accidentally left in

--- src/stretch/__init__.py
+++ src/stretch/__init__.py
@@ -4,6 +4,5 @@ from stretch.clients import StretchClient
 
 _client = StretchClient()
 for _name, _member in _inspect.getmembers(_client, _inspect.ismethod):
-    print(_name, _member)
     if not _name.startswith("_"):
         globals()[_name] = _member
