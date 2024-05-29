e57c23bd22a56a4a13aeb8a60c167a86424c87f1
Chris Paxton <cpaxton@hello-robot.com>
2024-05-29 14:11:59 -0400

    add recorder and test

---
+++ src/stretch/app/simple_recorder.py
@@ -0,0 +1,54 @@
+import click
+import numpy as np
+
+from stretch.agent import HomeRobotZmqClient
+from stretch.core import Parameters, get_parameters
+
+
+@click.command()
+@click.option("--robot_ip", default="192.168.1.15", help="IP address of the robot")
+@click.option("--recv_port", default=4401, help="Port to receive messages from the robot")
+@click.option("--send_port", default=4402, help="Port to send messages to the robot")
+@click.option(
+    "--parameter_file", default="config/default_planner.yaml", help="Path to parameter file"
+)
+def main(
+    robot_ip: str = "192.168.1.15",
+    recv_port: int = 4401,
+    send_port: int = 4402,
+    parameter_file: str = "config/default_planner.yaml",
+    device_id: int = 0,
+    verbose: bool = False,
+    show_intermediate_maps: bool = False,
+    reset: bool = False,
+):
+    """Set up the robot, create a task plan, and execute it."""
+    # Create robot
+    parameters = get_parameters(parameter_file)
+    robot = HomeRobotZmqClient(
+        robot_ip=robot_ip,
+        recv_port=recv_port,
+        send_port=send_port,
+        use_remote_computer=(not local),
+        parameters=parameters,
+    )
+
+    last_step = None
+    try:
+        # Loop and read in messages
+        # Write out RGB+D to file
+        while True:
+            obs = robot.get_observation()
+            # Add all unique observations
+            if obs["step"] != last_step:
+                last_step = obs["step"]
+                observations.append(obs)
+    finally:
+        # Disconnect from the robot
+        robot.stop()
+
+    breakpoint()
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/test/core/test_task.py
@@ -0,0 +1,73 @@
+from stretch.core.task import Operation, Task
+
+
+class Data:
+    def __init__(self, num: int = 0):
+        self.num = num
+
+
+class AddOperation(Operation):
+    def __init__(self, data, num, **kwargs):
+        super().__init__(f"+{num}", **kwargs)
+        self.data = data
+        self.num = num
+
+    def can_start(self) -> bool:
+        return True
+
+    def run(self) -> None:
+        print(f"Running {self.name}: {self.data.num} + {self.num} = {self.data.num + self.num}")
+        self.data.num += self.num
+
+    def was_successful(self) -> bool:
+        return True
+
+
+class ReturnIfEvenOperation(Operation):
+    def __init__(self, data, **kwargs):
+        super().__init__("Zero if even", **kwargs)
+        self.data = data
+
+    def can_start(self) -> bool:
+        print(f"Checking {self.name}: {self.data.num} % 2 == 0")
+        return self.data.num % 2 == 0
+
+    def run(self) -> None:
+        print(f"!!! {self.data.num} is even! !!!")
+
+    def was_successful(self) -> bool:
+        return True
+
+
+def test_task():
+    data = Data(0)
+    task = Task()
+
+    add1 = AddOperation(data, 1)
+    add2 = AddOperation(data, 2, parent=add1)
+    add3 = AddOperation(data, 3, parent=add2)
+    add1b = AddOperation(data, 1)
+    zero = ReturnIfEvenOperation(data, parent=add3, on_cannot_start=add1b)
+    add1b.on_success = zero
+
+    task.add_operation(add1)
+    task.add_operation(add2)
+    task.add_operation(add3)
+    task.add_operation(add1b)
+    task.add_operation(zero, terminal=True)
+
+    task.execute()
+    print("From 0:", data.num)
+
+    assert data.num == 6, "From 0, data should get transmuted into 6"
+
+    data.num = 1
+    task.start()
+
+    assert (
+        data.num == 8
+    ), "From 1, data should get transmuted into 8 - loops through + 1 an extra time"
+
+
+if __name__ == "__main__":
+    test_task()
