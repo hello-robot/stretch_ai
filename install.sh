11e375ca6db4cca67156220ae236f3794f6c428e
Chris Paxton <cpaxton@hello-robot.com>
2024-06-21 14:26:48 -0400

    remove dev

--- install.sh
+++ install.sh
@@ -67,7 +67,7 @@ pip install "git+https://github.com/facebookresearch/pytorch3d.git@stable"
 pip install torch_cluster -f https://pytorch-geometric.com/whl/torch-${PYTORCH_VERSION}+${CUDA_VERSION_NODOT}.html
 pip install torch_scatter -f https://pytorch-geometric.com/whl/torch-${PYTORCH_VERSION}+${CUDA_VERSION_NODOT}.html
 pip install torch_geometric
-pip install -e ./src[dev]
+pip install -e ./src
 # TODO: should we remove this?
 # Open3d is an optional dependency - not included in setup.py since not supported on 3.12
 # pip install open3d scikit-fmm
