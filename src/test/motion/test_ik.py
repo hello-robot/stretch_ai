cdb3f6be26b5e965c455866596635b7d105f91b8
Matt Lamsey <lamsey@hello-robot.com>
2024-06-25 18:13:48 -0400

    Add Cartesian IK + IK accuracy test (#15)
    * checkin new urdf and add ik accuracy test
    
    * checkin amal test
    
    * add missing __init__.py
    
    * run hooks
    
    * fix tests
    
    ---------
    
    Co-authored-by: mlamsey <31256490+mlamsey@users.noreply.github.com>

--- src/test/motion/test_ik.py
+++ src/test/motion/test_ik.py
@@ -10,7 +10,9 @@ import numpy as np
 
 from stretch.motion.pinocchio_ik_solver import PinocchioIKSolver
 
-true_all_joints: List[str] = [
+EPS_IK_CORRECT = 0.05  # max IK error
+
+true_all_joints_revolute: List[str] = [
     "joint_mobile_base_rotation",
     "joint_lift",
     "joint_arm_l0",
@@ -19,40 +21,62 @@ true_all_joints: List[str] = [
     "joint_wrist_roll",
 ]
 
+true_all_joints_translation: List[str] = [
+    "joint_mobile_base_translation",
+    "joint_lift",
+    "joint_arm_l0",
+    "joint_wrist_yaw",
+    "joint_wrist_pitch",
+    "joint_wrist_roll",
+]
+
+state_revolute = {
+    "joint_mobile_base_rotation": 0.0,
+    "joint_lift": 0.5,
+    "joint_arm_l0": 0.5,
+    "joint_wrist_pitch": 0,
+    "joint_wrist_yaw": 0,
+    "joint_wrist_roll": 0,
+}
+
+state_translation = {
+    "joint_mobile_base_translation": 0.0,
+    "joint_lift": 0.5,
+    "joint_arm_l0": 0.5,
+    "joint_wrist_pitch": 0,
+    "joint_wrist_yaw": 0,
+    "joint_wrist_roll": 0,
+}
+
 
 def test_fk_ik():
+    _test_fk_ik("stretch_base_rotation_ik.urdf", true_all_joints_revolute, state_revolute)
+    _test_fk_ik("stretch_base_translation_ik.urdf", true_all_joints_translation, state_translation)
+
+
+def _test_fk_ik(urdf_file, true_joint_names, initial_joint_state):
     # Create IK Solver
-    urdf_path = os.path.dirname(__file__) + "/stretch_base_rotation_ik.urdf"
+    try:
+        urdf_path = os.path.join(os.path.dirname(__file__), urdf_file)
+    except FileNotFoundError as e:
+        print(e)
+        assert False, "URDF file not found!"
+
     print("URDF path =", urdf_path)
-    ik_joints_allowed_to_move = [
-        "joint_arm_l0",
-        "joint_lift",
-        "joint_wrist_yaw",
-        "joint_wrist_pitch",
-        "joint_wrist_roll",
-        "joint_mobile_base_rotation",
-    ]
+    ik_joints_allowed_to_move = initial_joint_state.keys()
+
     manip_ik_solver = PinocchioIKSolver(
         urdf_path,
         "link_grasp_center",
         ik_joints_allowed_to_move,
     )
 
-    state = {
-        "joint_mobile_base_rotation": 0.0,
-        "joint_lift": 0.5,
-        "joint_arm_l0": 0.5,
-        "joint_wrist_pitch": 0,
-        "joint_wrist_yaw": 0,
-        "joint_wrist_roll": 0,
-    }
-
     all_joints = manip_ik_solver.get_all_joint_names()
-    for i, j in zip(all_joints, true_all_joints):
+    for i, j in zip(all_joints, true_joint_names):
         assert i == j, f"Joint name mismatch: {i} != {j}"
 
     # Test Forward Kinematics
-    ee_pose = manip_ik_solver.compute_fk(state)
+    ee_pose = manip_ik_solver.compute_fk(initial_joint_state)
     print(f"{ee_pose=}")
     assert ee_pose is not None, "FK failed"
 
@@ -62,11 +86,18 @@ def test_fk_ik():
     res, success, info = manip_ik_solver.compute_ik(
         ee_position,
         ee_orientation,
-        q_init=state,
+        q_init=initial_joint_state,
     )
     print("Result =", res)
     print("Success =", success)
     assert success, "IK failed"
+
+    # Test IK accuracy
+    res_ee_position, res_ee_orientation = manip_ik_solver.compute_fk(res)
+    ee_position_error = np.linalg.norm(res_ee_position - ee_position)
+    ee_orientation_error = np.linalg.norm(res_ee_orientation - ee_orientation)
+    assert ee_position_error < EPS_IK_CORRECT, "IK position error too large"
+    assert ee_orientation_error < EPS_IK_CORRECT, "IK orientation error too large"
 
     dt_sum = 0
     # Speed test
@@ -80,7 +111,7 @@ def test_fk_ik():
         res, success, info = manip_ik_solver.compute_ik(
             ee_position,
             ee_orientation,
-            q_init=state,
+            q_init=initial_joint_state,
         )
         t1 = timeit.default_timer()
         dt_sum += t1 - t0
@@ -200,8 +231,8 @@ def test_fk_various_links():
         print(f"Quaternion difference: {quat_diff}")
         print()
 
-        assert pos_diff < 0.05, "Position difference too large"
-        assert quat_diff < 0.05, "Quaternion difference too large"
+        assert pos_diff < EPS_IK_CORRECT, "Position difference too large"
+        assert quat_diff < EPS_IK_CORRECT, "Quaternion difference too large"
 
 
 def test_ik_restricted():
@@ -231,7 +262,7 @@ def test_ik_restricted():
     }
 
     all_joints = manip_ik_solver.get_all_joint_names()
-    for i, j in zip(all_joints, true_all_joints):
+    for i, j in zip(all_joints, true_all_joints_revolute):
         assert i == j, f"Joint name mismatch: {i} != {j}"
 
     # Test Forward Kinematics
