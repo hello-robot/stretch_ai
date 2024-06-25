c1761c0d0f3549254baa3a62299e9cb48afe69bf
Chris Paxton <cpaxton@hello-robot.com>
2024-06-25 11:59:10 -0400

    add updates

--- scripts/update.sh
+++ scripts/update.sh
@@ -6,6 +6,7 @@ echo "This will update Stretch AI requirements, especially stretch body."
 echo "Run this script on your robot, not on the development machine."
 echo "Fleet ID: $HELLO_FLEET_ID"
 echo "Fleet path: $HELLO_FLEET_PATH"
+echo "Ensure stretch_urdf is up-to-date before running this script."
 echo "---------------------------------------------"
 read -p "Does all this look correct? (y/n) " yn
 case $yn in
@@ -19,7 +20,9 @@ set -e
 pip install --upgrade hello-robot-stretch-body hello-robot-stretch-urdf
 
 echo "---------------------------------------------"
-echo "Going to Stretch URDF directory."
+echo "Running stretch_urdf commands."
+stretch_urdf_ros_update.py
+stretch_urdf_ros_update.py --ros2_rebuild
 
 echo "=============================================="
 echo "         UPDATE COMPLETE"
