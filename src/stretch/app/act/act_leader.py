c47fea6e6045920c6c8b5288dc6939ba908db6c7
Yi-Che (Jensen) Huang <yichehuang@hello-robot.com>
2024-06-27 18:34:57 -0400

    add modular observation preparation

--- src/stretch/app/act/act_leader.py
+++ src/stretch/app/act/act_leader.py
@@ -8,12 +8,13 @@ import cv2
 import numpy as np
 import torch
 import zmq
+from lerobot.common.datasets.push_dataset_to_hub.dobbe_format import clip_and_normalize_depth
 from lerobot.common.policies.act.modeling_act import ACTPolicy
 from lerobot.common.policies.diffusion.modeling_diffusion import DiffusionPolicy
 from torchvision.transforms import v2
 
 import stretch.utils.compression as compression
-from stretch.app.act.policy_utils import load_policy
+from stretch.app.act.policy_utils import load_policy, prepare_action_dict, prepare_observations
 from stretch.core import Evaluator
 from stretch.core.client import RobotClient
 from stretch.utils.data_tools.record import FileDataRecorder
@@ -64,8 +65,8 @@ class ACTLeader(Evaluator):
     def apply(self, message, display_received_images: bool = True) -> dict:
         """Take in image data and other data received by the robot and process it appropriately. Will parse the new observations, predict future actions and send the next action to the robot, and save everything to disk."""
 
-        color_image = compression.from_webp(message["ee_cam/color_image"])
-        depth_image = compression.unzip_depth(
+        gripper_color_image = compression.from_webp(message["ee_cam/color_image"])
+        gripper_depth_image = compression.unzip_depth(
             message["ee_cam/depth_image"], message["ee_cam/depth_image/shape"]
         )
         depth_camera_info = message["ee_cam/depth_camera_info"]
@@ -90,48 +91,41 @@ class ACTLeader(Evaluator):
         if self.camera is None:
             self.camera = Camera.from_K(
                 self.camera_info["camera_matrix"],
-                width=color_image.shape[1],
-                height=color_image.shape[0],
+                width=gripper_color_image.shape[1],
+                height=gripper_color_image.shape[0],
             )
 
+        # Rotate head camera images
+        head_depth_image = cv2.rotate(head_depth_image, cv2.ROTATE_90_CLOCKWISE)
+        head_color_image = cv2.rotate(head_color_image, cv2.ROTATE_90_CLOCKWISE)
+
         # Convert depth to meters
-        depth_image = depth_image.astype(np.float32) * self.depth_scale
+        gripper_depth_image = gripper_depth_image.astype(np.float32) * self.depth_scale
         head_depth_image = head_depth_image.astype(np.float32) * head_depth_scale
-        if self.display_point_cloud:
-            print("depth scale", self.depth_scale)
-            xyz = self.camera.depth_to_xyz(depth_image)
-            show_point_cloud(xyz, color_image / 255, orig=np.zeros(3))
 
+        # Clip and normalize depth
+        gripper_depth_image = clip_and_normalize_depth(gripper_depth_image, "gripper")
+        head_depth_image = clip_and_normalize_depth(head_depth_image, "head")
+
+        gripper_depth_image = np.stack((gripper_depth_image,) * 3, axis=-1)
+        head_depth_image = np.stack((head_depth_image,) * 3, axis=-1)
         if display_received_images:
-            # change depth to be h x w x 3
-            depth_image_x3 = np.stack((depth_image,) * 3, axis=-1)
-            combined = np.hstack((color_image / 255, depth_image_x3 / 4))
 
-            # Head images
-            head_depth_image = cv2.rotate(head_depth_image, cv2.ROTATE_90_CLOCKWISE)
-            head_color_image = cv2.rotate(head_color_image, cv2.ROTATE_90_CLOCKWISE)
-            head_depth_image_x3 = np.stack((head_depth_image,) * 3, axis=-1)
-            head_combined = np.hstack((head_color_image / 255, head_depth_image_x3 / 4))
+            # gripper_depth_image_obs = np.expand_dims(gripper_depth_image, axis=0)
+            # head_depth_image_obs = np.expand_dims(head_depth_image, axis=0)
 
-            # # Get the current height and width
-            # (height, width) = combined.shape[:2]
-            # (head_height, head_width) = head_combined.shape[:2]
+            # gripper_depth_image_obs = clip_and_normalize_depth(gripper_depth_image_obs, "gripper")
+            # head_depth_image_obs = clip_and_normalize_depth(head_depth_image_obs, "head")
 
-            # # Calculate the aspect ratio
-            # aspect_ratio = float(head_width) / float(head_height)
+            # gripper_depth_image_obs = np.squeeze(gripper_depth_image_obs, axis=0)
+            # head_depth_image_obs = np.squeeze(head_depth_image_obs, axis=0)
 
-            # # Calculate the new height based on the aspect ratio
-            # new_height = int(width / aspect_ratio)
-
-            # head_combined = cv2.resize(
-            #     head_combined, (width, new_height), interpolation=cv2.INTER_LINEAR
-            # )
-
-            # # Combine both images from ee and head
-            # combined = np.vstack((combined, head_combined))
-            # cv2.imshow("Observed RGB/Depth Image", combined)
-            cv2.imshow("gripper", color_image / 255)
-            cv2.imshow("head", head_color_image / 255)
+            gripper_combined = np.hstack((gripper_depth_image, gripper_color_image))
+            head_combined = np.hstack((head_depth_image, head_color_image))
+            # cv2.imshow("gripper_depth", gripper_depth_image_obs / 255)
+            # cv2.imshow("head_depth", head_depth_image_obs / 255)
+            cv2.imshow("gripper", gripper_combined)
+            cv2.imshow("head", head_combined)
 
         # Wait for spacebar to be pressed and start/stop recording
         # Spacebar is 32
@@ -178,68 +172,83 @@ class ACTLeader(Evaluator):
             self.goal_send_socket.send_pyobj(action_dict)
             return action_dict
 
-        if not self._recording:
-            # Process WASD keys for motion
-            if key == ord("w"):
-                xyt = np.array([0.2, 0.0, 0.0])
-            elif key == ord("a"):
-                xyt = np.array([0.0, 0.0, -np.pi / 8])
-            elif key == ord("s"):
-                xyt = np.array([-0.2, 0.0, 0.0])
-            elif key == ord("d"):
-                xyt = np.array([0.0, 0.0, np.pi / 8])
-
         action_dict = {}
         action = []
         if self._run_policy:
             # Build state observations in correct format
             raw_state = message["robot/config"]
-            state = np.array(
-                [
-                    raw_state["theta_vel"],
-                    raw_state["joint_lift"],
-                    raw_state["joint_arm_l0"],
-                    raw_state["joint_wrist_roll"],
-                    raw_state["joint_wrist_pitch"],
-                    raw_state["joint_wrist_yaw"],
-                    raw_state["stretch_gripper"],
-                ]
+
+            observations = prepare_observations(
+                raw_state,
+                gripper_color_image,
+                gripper_depth_image,
+                head_color_image,
+                head_depth_image,
+                "old_stationary_base",
+                self.device,
             )
 
-            state = torch.from_numpy(state)
+            # gripper_color_image_obs = torch.from_numpy(gripper_color_image)
+            # head_color_image_obs = torch.from_numpy(head_color_image)
+            # gripper_depth_image_obs = torch.from_numpy(gripper_depth_image)
+            # head_depth_image_obs = torch.from_numpy(head_depth_image)
 
-            gripper_color_image_obs = torch.from_numpy(color_image)
-            head_color_image_obs = torch.from_numpy(head_color_image)
+            # gripper_depth_image_obs = gripper_depth_image_obs.to(torch.float32) / 255
+            # gripper_depth_image_obs = gripper_depth_image_obs.permute(2, 0, 1)
 
-            state = state.to(torch.float32)
-            gripper_color_image_obs = gripper_color_image_obs.to(torch.float32) / 255
-            gripper_color_image_obs = gripper_color_image_obs.permute(2, 0, 1)
+            # head_depth_image_obs = head_depth_image_obs.to(torch.float32) / 255
+            # head_depth_image_obs = head_depth_image_obs.permute(2, 0, 1)
 
-            head_color_image_obs = head_color_image_obs.to(torch.float32) / 255
-            head_color_image_obs = head_color_image_obs.permute(2, 0, 1)
+            # gripper_color_image_obs = gripper_color_image_obs.to(torch.float32) / 255
+            # gripper_color_image_obs = gripper_color_image_obs.permute(2, 0, 1)
 
-            state = state.to(self.device, non_blocking=True)
-            gripper_color_image_obs = gripper_color_image_obs.to(self.device, non_blocking=True)
-            head_color_image_obs = head_color_image_obs.to(self.device, non_blocking=True)
+            # head_color_image_obs = head_color_image_obs.to(torch.float32) / 255
+            # head_color_image_obs = head_color_image_obs.permute(2, 0, 1)
 
-            transforms = v2.Compose([v2.CenterCrop(320)])
-            gripper_color_image_obs = transforms(gripper_color_image_obs)
-            head_color_image_obs = transforms(head_color_image_obs)
+            # state = state.to(self.device, non_blocking=True)
+            # gripper_color_image_obs = gripper_color_image_obs.to(self.device, non_blocking=True)
+            # head_color_image_obs = head_color_image_obs.to(self.device, non_blocking=True)
+            # gripper_depth_image_obs = gripper_depth_image_obs.to(self.device, non_blocking=True)
+            # head_depth_image_obs = head_depth_image_obs.to(self.device, non_blocking=True)
 
-            # Add extra (empty) batch dimension, required to forward the policy
-            state = state.unsqueeze(0)
-            head_color_image_obs = head_color_image_obs.unsqueeze(0)
-            gripper_color_image_obs = gripper_color_image_obs.unsqueeze(0)
+            # transforms = v2.Compose([v2.CenterCrop(320)])
+            # gripper_color_image_obs = transforms(gripper_color_image_obs)
+            # head_color_image_obs = transforms(head_color_image_obs)
+            # gripper_depth_image_obs = transforms(gripper_depth_image_obs)
+            # head_depth_image_obs = transforms(head_depth_image_obs)
+
+            # # Add extra (empty) batch dimension, required to forward the policy
+            # state = state.unsqueeze(0)
+            # head_color_image_obs = head_color_image_obs.unsqueeze(0)
+            # gripper_color_image_obs = gripper_color_image_obs.unsqueeze(0)
+            # gripper_depth_image_obs = gripper_depth_image_obs.unsqueeze(0)
+            # head_depth_image_obs = head_depth_image_obs.unsqueeze(0)
 
             # Build observation dict for ACT
-            observation = {
-                "observation.state": state,
-                "observation.images.gripper": gripper_color_image_obs,
-                "observation.images.head": head_color_image_obs,
-            }
+            # observation = {
+            #     "observation.state": state,
+            #     "observation.images.gripper": gripper_color_image_obs,
+            #     "observation.images.head": head_color_image_obs,
+            # }
+
+            # observations = {
+            #     "observation.state": state,
+            #     "observation.images.gripper": gripper_color_image_obs,
+            #     "observation.images.head": head_color_image_obs,
+            #     "observation.images.gripper_depth": gripper_depth_image_obs,
+            #     "observation.images.head_depth": head_depth_image_obs,
+            # }
+
+            # observations = {
+            #     k: v.to(self.device, non_blocking=True) for k, v in observations.items()
+            # }
+
+            for x in observations:
+                print(observations[x].shape)
+                print(type(observations[x]))
             # Send observation to policy
             with torch.inference_mode():
-                raw_action = self.policy.select_action(observation)
+                raw_action = self.policy.select_action(observations)
 
             # Get first batch
             action = raw_action[0].tolist()
@@ -263,8 +272,8 @@ class ACTLeader(Evaluator):
 
             # Record episode if enabled
             self._recorder.add(
-                ee_rgb=color_image,
-                ee_depth=depth_image,
+                ee_rgb=gripper_color_image,
+                ee_depth=gripper_depth_image,
                 xyz=np.array([0]),
                 quaternion=np.array([0]),
                 gripper=0,
--- src/stretch/app/act/policy_utils.py
+++ src/stretch/app/act/policy_utils.py
@@ -1,13 +1,17 @@
+import numpy as np
 import torch
 from lerobot.common.policies.act.modeling_act import ACTPolicy
 from lerobot.common.policies.diffusion.modeling_diffusion import DiffusionPolicy
+from torchvision.transforms import v2
 
-SUPPORTED = ["act", "diffusion"]
+SUPPORTED_POLICIES = ["act", "diffusion"]
+SUPPORTED_MODES = ["rotary_base", "stationary_base", "base_x"]
 
 
-def load_policy(policy_name, policy_path, device):
-
-    # Checks if model is supported, then loads model
+def load_policy(
+    policy_name: str | None = None, policy_path: str | None = None, device: str | None = "cuda"
+):
+    """Loads specified policy with name and path. Current supported policies include 'act', 'diffusion'"""
     policy = None
     if policy_name == "act":
         policy = ACTPolicy.from_pretrained(policy_path)
@@ -15,9 +19,114 @@ def load_policy(policy_name, policy_path, device):
         policy = DiffusionPolicy.from_pretrained(policy_path)
     else:
         raise NotImplementedError(
-            f"{policy_name} is not a supported policy. Supported policies: {SUPPORTED}"
+            f"{policy_name} is not a supported policy. Supported policies: {SUPPORTED_POLICIES}"
         )
     policy.to(device)
     policy.eval()
 
     return policy
+
+
+def prepare_state(
+    raw_state: dict | None = None, teleop_mode: str | None = None, device: str | None = "cuda"
+):
+    state = None
+    if teleop_mode == "rotary_base":
+        state = [
+            raw_state["theta"],
+            0.0,  # Placeholder 0 for base_x
+            raw_state["joint_lift"],
+            raw_state["joint_arm_l0"],
+            raw_state["joint_wrist_roll"],
+            raw_state["joint_wrist_pitch"],
+            raw_state["joint_wrist_yaw"],
+            raw_state["stretch_gripper"],
+        ]
+    elif teleop_mode == "stationary_base":
+        state = [
+            0.0,  # Placeholder 0 for theta
+            0.0,  # Placeholder 0 for base_x
+            raw_state["joint_lift"],
+            raw_state["joint_arm_l0"],
+            raw_state["joint_wrist_roll"],
+            raw_state["joint_wrist_pitch"],
+            raw_state["joint_wrist_yaw"],
+            raw_state["stretch_gripper"],
+        ]
+    elif teleop_mode == "base_x":
+        state = [
+            0.0,  # Placeholder 0 for theta
+            raw_state["base_x"],
+            raw_state["joint_lift"],
+            raw_state["joint_arm_l0"],
+            raw_state["joint_wrist_roll"],
+            raw_state["joint_wrist_pitch"],
+            raw_state["joint_wrist_yaw"],
+            raw_state["stretch_gripper"],
+        ]
+    # TODO remove old_stationary_base that only has 7 features (for compatibility with old models)
+    elif teleop_mode == "old_stationary_base":
+        state = [
+            0.0,  # Placeholder 0 for theta
+            raw_state["joint_lift"],
+            raw_state["joint_arm_l0"],
+            raw_state["joint_wrist_roll"],
+            raw_state["joint_wrist_pitch"],
+            raw_state["joint_wrist_yaw"],
+            raw_state["stretch_gripper"],
+        ]
+    else:
+        raise NotImplementedError(
+            f"{teleop_mode} is not a supported teleop mode. Supported modes: {SUPPORTED_MODES}"
+        )
+    state = torch.from_numpy(np.array(state))
+    state = state.to(torch.float32)
+    state = state.to(device, non_blocking=True)
+    state = state.unsqueeze(0)
+
+    return state
+
+
+def prepare_image(image, device):
+
+    transforms = v2.Compose([v2.CenterCrop(320)])
+    image = torch.from_numpy(image)
+    image = image.to(torch.float32) / 255
+    image = image.permute(2, 0, 1)
+    image = image.to(device, non_blocking=True)
+    image = transforms(image)
+    image = image.unsqueeze(0)
+
+    return image
+
+
+def prepare_observations(
+    raw_state: dict | None = None,
+    gripper_color_image: np.ndarray | None = None,
+    gripper_depth_image: np.ndarray | None = None,
+    head_color_image: np.ndarray | None = None,
+    head_depth_image: np.ndarray | None = None,
+    teleop_mode: str | None = "stationary_base",
+    device: str | None = "cuda",
+):
+    """Prepares joint state observations according to teleop mode. Current supported modes include 'rotary_base', 'stationary_base', and 'base_x'"""
+    state = prepare_state(raw_state, teleop_mode, device)
+    images = [gripper_color_image, gripper_depth_image, head_color_image, head_depth_image]
+    gripper_color_image, gripper_depth_image, head_color_image, head_depth_image = [
+        prepare_image(x, device) for x in images
+    ]
+
+    observations = {
+        "observation.state": state,
+        "observation.images.gripper": gripper_color_image,
+        "observation.images.head": head_color_image,
+        "observation.images.gripper_depth": gripper_depth_image,
+        "observation.images.head_depth": head_depth_image,
+    }
+
+    return observations
+
+
+def prepare_action_dict():
+    # TODO
+    return
