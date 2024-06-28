104c6f87d4210692819f707928f47d9ba4b1f183
Chris Paxton <cpaxton@hello-robot.com>
2024-06-28 17:44:57 -0400

    update

--- src/test/utils/test_pytorch3d_utils.py
+++ src/test/utils/test_pytorch3d_utils.py
@@ -16,23 +16,24 @@ def _make_box(l1, l2, l3):
     box[0, 6, :] = torch.tensor([-1.0, 1.0, -1.0])
     box[0, 7, :] = torch.tensor([-1.0, -1.0, -1.0])
 
-    box[0, :, 0] *= l1 / 2.
-    box[0, :, 1] *= l2 / 2.
-    box[0, :, 2] *= l3 / 2.
+    box[0, :, 0] *= l1 / 2.0
+    box[0, :, 1] *= l2 / 2.0
+    box[0, :, 2] *= l3 / 2.0
 
     return box
 
-def _make_boxes(N, M, x_min=1.):
+
+def _make_boxes(N, M, x_min=1.0):
     b1 = torch.zeros([N, 8, 3])
     b2 = torch.zeros([M, 8, 3])
 
     for i in range(N):
-        x = x_min + 0.1*i
-        b1[i, :, :] = _make_box(x, x ,x)
+        x = x_min + 0.1 * i
+        b1[i, :, :] = _make_box(x, x, x)
 
     for i in range(M):
-        x = x_min + 0.1*i
-        b2[i, :, :] = _make_box(x, x ,x)
+        x = x_min + 0.1 * i
+        b2[i, :, :] = _make_box(x, x, x)
 
     return b1, b2
 
@@ -43,17 +44,17 @@ def test_box3d_overlap():
 
     # check VoI / IOU
     # box: n (#) x 8 (corners) x 3 (XYZ)
-    boxes1, boxes2 = _make_boxes(N, M, x_min=1.)
+    boxes1, boxes2 = _make_boxes(N, M, x_min=1.0)
     vol, iou = box3d_overlap(boxes1, boxes2)
 
     print("VoI:", vol)
     print("IoU:", iou)
-    assert(vol.shape == torch.Size([2, 3]))
-    assert(iou.shape == torch.Size([2, 3]))
+    assert vol.shape == torch.Size([2, 3])
+    assert iou.shape == torch.Size([2, 3])
 
-    boxes1, _ = _make_boxes(1, 1, x_min=2.)
+    boxes1, _ = _make_boxes(1, 1, x_min=2.0)
     box_volume = _box3d_overlap.volume(boxes1)
-    assert box_volume == torch.tensor(8.)
+    assert box_volume == torch.tensor(8.0)
 
 
 def _test_all():
