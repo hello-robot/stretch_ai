346eb216db1cb174509bf5fd0ab5084dd889a068
Chris Paxton <cpaxton@hello-robot.com>
2024-06-28 16:31:14 -0400

    update

--- src/test/utils/test_pytorch3d_utils.py
+++ src/test/utils/test_pytorch3d_utils.py
@@ -1,18 +1,19 @@
-from stretch.utils.pytorch3d_helpers import box3d_overlap
-
 import torch
+
+from stretch.utils.pytorch3d import box3d_overlap
+
 
 def _make_box(l1, l2, l3):
     """l: lengths of sides"""
     box = torch.zeros([1, 8, 3])
-    box[0, 0, :] = torch.tensor([1., -1., 1.])
-    box[0, 1, :] = torch.tensor([1., 1., 1.])
-    box[0, 2, :] = torch.tensor([1., 1., -1.])
-    box[0, 3, :] = torch.tensor([1., -1., -1.])
-    box[0, 4, :] = torch.tensor([-1., -1., 1.])
-    box[0, 5, :] = torch.tensor([-1., 1., 1.])
-    box[0, 6, :] = torch.tensor([-1., 1., -1.])
-    box[0, 7, :] = torch.tensor([-1., -1., -1.])
+    box[0, 0, :] = torch.tensor([1.0, -1.0, 1.0])
+    box[0, 1, :] = torch.tensor([1.0, 1.0, 1.0])
+    box[0, 2, :] = torch.tensor([1.0, 1.0, -1.0])
+    box[0, 3, :] = torch.tensor([1.0, -1.0, -1.0])
+    box[0, 4, :] = torch.tensor([-1.0, -1.0, 1.0])
+    box[0, 5, :] = torch.tensor([-1.0, 1.0, 1.0])
+    box[0, 6, :] = torch.tensor([-1.0, 1.0, -1.0])
+    box[0, 7, :] = torch.tensor([-1.0, -1.0, -1.0])
 
     box[0, :, 0] *= l1
     box[0, :, 1] *= l2
@@ -20,19 +21,21 @@ def _make_box(l1, l2, l3):
 
     return box
 
+
 def _make_boxes(N, M):
     b1 = torch.zeros([N, 8, 3])
     b2 = torch.zeros([M, 8, 3])
 
     for i in range(N):
-        x = 0.5 + 0.1*i
-        b1[i, :, :] = _make_box(x, x ,x)
+        x = 0.5 + 0.1 * i
+        b1[i, :, :] = _make_box(x, x, x)
 
     for i in range(M):
-        x = 0.5 + 0.1*i
-        b2[i, :, :] = _make_box(x, x ,x)
+        x = 0.5 + 0.1 * i
+        b2[i, :, :] = _make_box(x, x, x)
 
     return b1, b2
+
 
 def test_box3d_overlap():
     N = 2
@@ -46,8 +49,10 @@ def test_box3d_overlap():
     print("VoI:", vol)
     print("IoU:", iou)
 
+
 def _test_all():
     test_box3d_overlap()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     _test_all()
