3ceba17eb6c4af070fbb439064e26cf38d5e1733
Chris Paxton <cpaxton@hello-robot.com>
2024-07-01 17:10:04 -0400

    information

---
+++ src/stretch/app/print_joint_states.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+
+import time
+
+import click
+import cv2
+import numpy as np
+
+from stretch.agent.zmq_client import HomeRobotZmqClient
+from stretch.core import Parameters, get_parameters
+from stretch.motion import HelloStretchIdx
+
+
+@click.command()
+@click.option("--robot_ip", default="192.168.1.15", help="IP address of the robot")
+@click.option(
+    "--local",
+    is_flag=True,
+    help="Set if we are executing on the robot and not on a remote computer",
+)
+@click.option("--parameter_file", default="default_planner.yaml", help="Path to parameter file")
+def main(
+    robot_ip: str = "192.168.1.15",
+    local: bool = False,
+    parameter_file: str = "config/default_planner.yaml",
+):
+    # Create robot
+    parameters = get_parameters(parameter_file)
+    robot = HomeRobotZmqClient(
+        robot_ip=robot_ip,
+        use_remote_computer=(not local),
+        parameters=parameters,
+    )
+    robot.start()
+    try:
+        while True:
+            joint_state = robot.get_joint_state()
+            if joint_state is None:
+                continue
+            print(
+                f"Arm: {joint_state[HelloStretchIdx.ARM]}, Lift: {joint_state[HelloStretchIdx.LIFT]}, Gripper: {joint_state[HelloStretchIdx.GRIPPER]}"
+            )
+            time.sleep(0.01)
+
+    except KeyboardInterrupt:
+        pass
+    robot.stop()
+
+
+if __name__ == "__main__":
+    main()
