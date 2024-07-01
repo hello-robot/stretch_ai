1d13c63b60d9a05f9ad6fee7ea220e8cd2eaaa57
Chris Paxton <cpaxton@hello-robot.com>
2024-07-01 12:08:17 -0400

    update documentation

--- scripts/build-docker.sh
+++ scripts/build-docker.sh
@@ -9,6 +9,7 @@ else
     echo "Exiting..."
     exit 1
 fi
+# Build the docker image with the current tag.
 docker build -t hellorobotinc/stretch-ai_cuda-11.8:$VERSION .
 docker push hellorobotinc/stretch-ai_cuda-11.8:$VERSION
 docker tag hellorobotinc/stretch-ai_cuda-11.8:$VERSION hellorobotinc/stretch-ai_cuda-11.8:latest
