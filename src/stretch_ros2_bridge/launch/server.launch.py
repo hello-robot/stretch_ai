ed9698e84339ee0c27ce1fed3091138f02f0159d
Chris Paxton <cpaxton@hello-robot.com>
2024-07-12 11:24:31 -0700

    renaming some files

--- src/stretch_ros2_bridge/launch/server.launch.py
+++ src/stretch_ros2_bridge/launch/server.launch.py
@@ -22,7 +22,7 @@ def generate_launch_description():
         PythonLaunchDescriptionSource(
             os.path.join(
                 get_package_share_directory("stretch_ros2_bridge"),
-                "launch/startup_stretch_orbslam.launch.py",
+                "launch/startup_stretch_hector_slam.launch.py",
             )
         )
     )
