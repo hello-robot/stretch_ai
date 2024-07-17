3582a678c8116358847c609fa0a308aaef93dda2
Chris Paxton <cpaxton@hello-robot.com>
2024-07-17 06:24:30 -0400

    update

--- src/stretch/motion/utils/bullet.py
+++ src/stretch/motion/utils/bullet.py
@@ -6,12 +6,12 @@ from typing import Optional, Tuple
 
 import numpy as np
 
+# TODO: resolve this
+import pybullet as pb
+
 from stretch.motion.base.ik_solver_base import IKSolverBase
 from stretch.motion.robot import RobotModel
 from stretch.utils.bullet import PbArticulatedObject, PbClient
-
-# TODO: resolve this
-# import pybullet as pb
 
 
 class BulletRobotModel(RobotModel):
