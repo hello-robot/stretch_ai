7fd5c716670d855036401e00598471299b2d24a8
Mohamed Fazil <mfazil@hello-robot.com>
2024-07-18 01:19:37 -0700

    Head camera pinhole model working but transform issue

--- src/stretch/visualization/rerun.py
+++ src/stretch/visualization/rerun.py
@@ -5,6 +5,13 @@ import rerun as rr  # pip install rerun-sdk
 import cv2
 
 
+def decompose_homogeneous_matrix(homogeneous_matrix):
+    if homogeneous_matrix.shape != (4, 4):
+        raise ValueError("Input matrix must be 4x4")
+    rotation_matrix = homogeneous_matrix[:3, :3]
+    translation_vector = homogeneous_matrix[:3, 3]
+    return rotation_matrix, translation_vector
+
 class RerunVsualizer:
     def __init__(self) -> None:
 
@@ -23,19 +30,32 @@ class RerunVsualizer:
         # Log a bounding box as a visual placeholder for the map
         rr.log(
             "map/box",
-            rr.Boxes3D(half_sizes=[4, 4, 1], centers=[0, 0, 1], colors=[255, 255, 255, 255]),
+            rr.Boxes3D(half_sizes=[10, 10, 3], centers=[0, 0, 2], colors=[255, 255, 255, 255]),
             static=True,
         )
     def step(self, obs):
         if obs:
             try:
+                # Robot World pose
                 xy = obs['gps']
                 theta = obs['compass']
-                print(f"Logging XYT: {xy},{theta}")
                 arrow = rr.Arrows3D(origins=[0, 0, 0], vectors=[1, 0, 0], radii=0.02, labels="robot", colors=[255, 0, 0, 255])
                 rr.log("world/robot/arrow", arrow)
                 rr.log("world/robot/blob", rr.Points3D([0,0,0], colors=[255, 0, 0, 255], radii=0.175))
                 rr.log("world/robot", rr.Transform3D(translation=[xy[0], xy[1], 0], rotation=rr.RotationAxisAngle(axis=[0,0,1],radians=theta)))
+
+                # Head Image
+                rr.log("world/robot/head_camera/img", rr.Image(obs["rgb"]))
+                rr.log("world/robot/head_camera/depth", rr.DepthImage(obs["depth"], meter=1000.0, colormap="viridis"))
+                rr.log(
+                    "world/robot/head_camera/img",
+                    rr.Pinhole(
+                        resolution=[obs["rgb"].shape[1], obs["rgb"].shape[0]],
+                        image_from_camera=obs['camera_K'],
+                    ),
+                )
+                rot,trans= decompose_homogeneous_matrix(obs['camera_pose'])
+                rr.log("world/robot/head_camera", rr.Transform3D(translation=trans, mat3x3=rot))
             except Exception as e:
-                pass
+                print(e)
         
