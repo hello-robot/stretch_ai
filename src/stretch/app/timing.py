5b68056f5a4ae2df31f5f8aa5c16ea434fce5816
Chris Paxton <cpaxton@hello-robot.com>
2024-07-25 13:43:40 -0400

    updates to timing

--- src/stretch/app/timing.py
+++ src/stretch/app/timing.py
@@ -4,6 +4,8 @@ import os
 import sys
 
 import click
+import cv2
+import numpy as np
 
 from stretch.agent.zmq_client import HomeRobotZmqClient
 from stretch.utils import LoopStats
@@ -30,6 +32,29 @@ def main(robot_ip: str = "192.168.1.15", local: bool = False, headless: bool = F
     while True:
         loop.mark_start()
         observation = robot.get_servo_observation()
+
+        if not headless:
+            # Get servo observation from the robot
+            # This is a more compact, lower-res image representation for better performance over network
+            observation = robot.get_servo_observation()
+
+            # Get RGB and Depth
+            rgb = observation.rgb
+            depth = observation.depth
+
+            # Convert depth to a color image using OpenCV
+            depth = cv2.applyColorMap(cv2.convertScaleAbs(depth, alpha=0.03), cv2.COLORMAP_JET)
+
+            # RGB to BGR for OpenCV
+            rgb = cv2.cvtColor(rgb, cv2.COLOR_RGB2BGR)
+
+            # Concatenate rgb and depth so they are side-by-side
+            combined = np.concatenate((rgb, depth), axis=1)
+
+            # Show the image
+            cv2.imshow("Robot View", combined)
+            cv2.waitKey(1)
+
         loop.mark_end()
         loop.pretty_print()
         counter += 1
