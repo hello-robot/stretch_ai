512dad571407d70313817520fd2c0b7e08dde000
Chris Paxton <cpaxton@hello-robot.com>
2024-07-25 13:07:42 -0400

    update things a bit

--- src/stretch/app/keyboard_teleop.py
+++ src/stretch/app/keyboard_teleop.py
@@ -4,6 +4,7 @@ import os
 import sys
 
 import click
+import cv2
 import numpy as np
 
 from stretch.agent.zmq_client import HomeRobotZmqClient
@@ -60,7 +61,8 @@ def getch():
     is_flag=True,
     help="Set if we are executing on the robot and not on a remote computer",
 )
-def main(robot_ip: str = "192.168.1.15", local: bool = False):
+@click.option("--headless", is_flag=True, help="Do not show camera feeds")
+def main(robot_ip: str = "192.168.1.15", local: bool = False, headless: bool = False):
 
     # Create robot
     robot = HomeRobotZmqClient(
@@ -72,14 +74,41 @@ def main(robot_ip: str = "192.168.1.15", local: bool = False):
 
     print("Press W, A, S, or D. Press 'q' to quit.")
     while True:
-        char = getch()
+
+        if not headless:
+            # Get servo observation from the robot
+            # This is a more compact, lower-res image representation for better performance over network
+            observation = robot.get_servo_observation()
+
+            # Get RGB and Depth
+            rgb = observation.rgb
+            depth = observation.depth
+
+            # Convert depth to a color image using OpenCV
+            depth = cv2.applyColorMap(cv2.convertScaleAbs(depth, alpha=0.03), cv2.COLORMAP_JET)
+
+            # RGB to BGR for OpenCV
+            rgb = cv2.cvtColor(rgb, cv2.COLOR_RGB2BGR)
+
+            # Concatenate rgb and depth so they are side-by-side
+            combined = np.concatenate((rgb, depth), axis=1)
+
+            # Show the image
+            cv2.imshow("Robot View", combined)
+            char = chr(cv2.waitKey(1) & 0xFF)  # 0xFF is a mask to get the last 8 bits
+        else:
+            char = getch()
+
         if char == "q":
             robot.stop()
             break
         elif char in ["w", "a", "s", "d"]:
             key_pressed(robot, char)
+        elif headless and char == chr(255):
+            # Nothing was pressed
+            pass
         else:
-            print("Invalid input. Please press W, A, S, or D. Press 'q' to quit.")
+            print(f"Invalid input {char}. Please press W, A, S, or D. Press 'q' to quit.")
 
 
 if __name__ == "__main__":
