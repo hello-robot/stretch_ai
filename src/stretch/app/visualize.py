a2e410ad3e51587206f71ff113fffdcd1bd5c0c1
Chris Paxton <cpaxton@hello-robot.com>
2024-07-29 13:58:01 -0400

    add visualization server

---
+++ src/stretch/app/visualize.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+
+import time
+
+import click
+import cv2
+import numpy as np
+
+from stretch.agent.zmq_client import HomeRobotZmqClient
+from stretch.motion import HelloStretchIdx
+
+
+@click.command()
+@click.option("--robot_ip", default="", help="IP address of the robot")
+@click.option(
+    "--local",
+    is_flag=True,
+    help="Set if we are executing on the robot and not on a remote computer",
+)
+def main(
+    robot_ip: str = "192.168.1.15",
+    local: bool = False,
+    parameter_file: str = "config/default_planner.yaml",
+):
+    # Create robot
+    robot = HomeRobotZmqClient(
+        robot_ip=robot_ip,
+        use_remote_computer=(not local),
+        enable_rerun_server=True,
+    )
+    robot.start()
+    try:
+        while True:
+            time.sleep(0.01)
+    except KeyboardInterrupt:
+        pass
+    robot.stop()
+
+
+if __name__ == "__main__":
+    main()
