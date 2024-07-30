0cd19517b3ad6f22c6ff4c475a3293679a295aa6
Chris Paxton <cpaxton@hello-robot.com>
2024-07-30 17:09:30 -0400

    speech to text model

---
+++ src/stretch/audio/speech_to_text.py
@@ -0,0 +1,18 @@
+import whisper
+
+
+class WhisperSpeechToText:
+    """Use the whisper model to transcribe speech to text."""
+
+    def __init__(self):
+        self.model = whisper.load_model("base")
+
+    def transcribe(self, audio_file: str) -> str:
+        """Transcribe the audio file to text."""
+        result = self.model.transcribe(audio_file)
+        return result["text"]
+
+
+if __name__ == "__main__":
+    wst = WhisperSpeechToText()
+    print(wst.transcribe("test.wav"))
