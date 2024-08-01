1bb5d0ac85e00414175efdf6c5bacf2f7b5cf0f1
Chris Paxton <165678659+hello-cpaxton@users.noreply.github.com>
2024-08-01 09:23:11 -0400

    Add self hosted runner (#66)
    * add self hosted runner
    
    * add
    
    * update
    
    * another update to OS
    
    * fix runner
    
    * update labels
    
    * update run install script
    
    * no remove
    
    * update
    
    * add version setup
    
    * update install script
    
    * installation script
    
    * updates to isntall script - make conda work?
    
    * no install
    
    * remove some stuff from install
    
    * remove other versions so we dont run so many jobs
    
    * update with no submodules flag
    
    * add types of PRs
    
    * update - remove some dead code
    
    * run precommit the other way
    
    * testing setup
    
    * do not install
    
    * no matrix
    
    * updates
    
    * a couple more tests
    
    * python version setup not necessary
    
    * install precommit
    
    * fix list
    
    * no mamba
    
    * where are we
    
    * update info
    
    * precommit hooks
    
    * some more updates
    
    * update
    
    * fix invalid file
    
    * some fixes
    
    * update
    
    * update
    
    * update
    
    * pwd
    
    * needs mypy
    
    * missing functions
    
    * flake8 is out
    
    * pytest
    
    * pytest setup
    
    * pips and pythons
    
    * update
    
    * those are only for dev
    
    * comment out loopstats execution time test - running on our computers so
    its slow/unreliabel

--- install.sh
+++ install.sh
@@ -8,6 +8,8 @@ CUDA_VERSION_NODOT="${CUDA_VERSION//./}"
 export CUDA_HOME=/usr/local/cuda-$CUDA_VERSION
 
 CPU_ONLY="false"
+NO_REMOVE="false"
+NO_SUBMODULES="false"
 INSTALL_PYTORCH3D="false"
 MAMBA=mamba
 # Two cases: -y for yes, --cpu for cpu only
@@ -32,6 +34,14 @@ do
             INSTALL_PYTORCH3D="true"
             shift
             ;;
+        --no-remove)
+            NO_REMOVE="true"
+            shift
+            ;;
+        --no-submodules)
+            NO_SUBMODULES="true"
+            shift
+            ;;
         *)
             shift
             # unknown option
@@ -39,15 +49,16 @@ do
     esac
 done
 
+VERSION="_0.0.5"
 # If cpu only, set the cuda version to cpu
 if [ "$CPU_ONLY" == "true" ]; then
     export CUDA_VERSION=cpu
     export CUDA_VERSION_NODOT=cpu
     export CUDA_HOME=""
-    ENV_NAME=stretch_ai_cpu
+    ENV_NAME=stretch_ai_cpu$VERSION
 else
     export CUDA_VERSION_NODOT="${CUDA_VERSION//./}"
-    ENV_NAME=stretch_ai
+    ENV_NAME=stretch_ai$VERSION
 fi
 
 echo "=============================================="
@@ -99,7 +110,11 @@ fi
 # Exit immediately if anything fails
 set -e
 
-$MAMBA env remove -n $ENV_NAME -y
+# Only remove if NO_REMOVe is false
+if [ "$NO_REMOVE" == "false" ]; then
+    echo "Removing existing environment..."
+    $MAMBA env remove -n $ENV_NAME -y
+fi
 # If using cpu only, create a separate environment
 if [ "$CPU_ONLY" == "true" ]; then
     $MAMBA create -n $ENV_NAME -c pyg -c pytorch pytorch=$PYTORCH_VERSION pyg torchvision cpuonly python=$PYTHON_VERSION -y
@@ -109,6 +124,10 @@ else
 fi
 
 source activate $ENV_NAME
+# conda activate $ENV_NAME
+#echo "Activating environment... `$MAMBA info --base`/envs/$ENV_NAME/bin/activate"
+#source `$MAMBA info --base`/envs/$ENV_NAME/bin/activate
+#echo "activated"
 
 # Now install pytorch3d a bit faster
 $MAMBA install -c fvcore -c iopath -c conda-forge fvcore iopath -y
@@ -153,7 +172,8 @@ else
 fi
 
 # If not cpu only, then we can use perception
-if [ "$CPU_ONLY" == "true" ]; then
+# OR if no submodules, then we can't install perception
+if [ "$CPU_ONLY" == "true" ] || [ "$NO_SUBMODULES" == "true" ]; then
     echo "Skipping perception installation for CPU only"
 else
     echo "Install detectron2 for perception (required by Detic)"
