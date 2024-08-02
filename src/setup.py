36a6a2ab0481ba44003b0185b54f9f0021005208
Chris Paxton <cpaxton@hello-robot.com>
2024-08-02 08:47:38 -0400

    update login

--- src/setup.py
+++ src/setup.py
@@ -32,7 +32,6 @@ setuptools.setup(
         "openai-clip",
         "yacs",
         "loguru",
-        "transformers",
         "scikit-image",
         "pybullet",
         "sophuspy",
@@ -52,6 +51,7 @@ setuptools.setup(
         "hydra-core",
         "timm",
         "huggingface_hub[cli]",
+        "transformers",
         # Compression tools
         "pyliblzfse",
         "webp",
