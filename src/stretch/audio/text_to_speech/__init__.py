e52aa22707e8ab1f3af64a159f132a4de1ff819f
Chris Paxton <cpaxton@hello-robot.com>
2024-08-07 12:00:40 -0400

    Update TTS

--- src/stretch/audio/text_to_speech/__init__.py
+++ src/stretch/audio/text_to_speech/__init__.py
@@ -2,6 +2,7 @@ import logging
 from typing import Optional
 
 from .executor import AbstractTextToSpeech, TextToSpeechExecutor, TextToSpeechOverrideBehavior
+from .google_cloud_engine import GoogleCloudTextToSpeech
 from .gtts_engine import GTTSTextToSpeech
 from .pyttsx3_engine import PyTTSx3TextToSpeech
 
@@ -24,8 +25,6 @@ def get_text_to_speech(name: str) -> AbstractTextToSpeech:
     """
     name = name.lower()
     if name == "google":
-        from .google_cloud_engine import GoogleCloudTextToSpeech
-
         return GoogleCloudTextToSpeech()
     if name == "gtts":
         return GTTSTextToSpeech()
