ac4323cdb6a3731283cfd01b9cda647e94b5ed03
hello-amal <amaln@hello-robot.com>
2024-08-07 07:42:00 -0700

    Add Text-to-Speech Implementations & CLI App (#57)
    * Added TTS
    
    * Update setup.py
    
    * Install `libasound2-dev` in workflow
    
    * Github workflows require `sudo apt-get` installs
    
    * Add portaudio to the `apt-get` installs
    
    * Fixes from pre-commits
    
    * Add espeak to github actions installation
    
    * Remove mp3s
    
    * Configure LFS to track MP3s
    
    * Added a check for large files in the pre-commit
    
    * Changes from PR review
    
    * Update github actions dep to fake audio capabilities
    
    * Update the apt install
    
    * updates to docker
    
    * workflow updates
    
    * Add espeak to README audio deps
    
    * Add ffmpeg
    
    * [WIP] list audioread backends in github actions
    
    * Refactor available formats
    
    * Implemented GoogleCloudTTS
    
    * [WIP] list audioread backends in github actions
    
    * [WIP] add verbose logs to failing test case
    
    * Remove GoogleCloudTTS on GithubActions
    
    * [WIP] verify the named temp file has size > 0
    
    * [WIP] check if FFMPeg gets a decoder error on the mp3s
    
    * Pull LFS files in Github Action
    
    * Add Git LFS to the action workflow
    
    * Mark the git directory as safe before pulling LFS files
    
    * Move git-lfs from action workflow to docker file
    
    * Re-trigger github actions
    
    ---------
    
    Co-authored-by: Amal Nanavati <amaln@uw.edu>
    Co-authored-by: Chris Paxton <cpaxton@hello-robot.com>
    Co-authored-by: Chris Paxton <165678659+hello-cpaxton@users.noreply.github.com>

---
+++ src/stretch/audio/text_to_speech/__init__.py
@@ -0,0 +1,4 @@
+from .executor import TextToSpeechExecutor, TextToSpeechOverrideBehavior
+from .google_cloud_engine import GoogleCloudTextToSpeech
+from .gtts_engine import GTTSTextToSpeech
+from .pyttsx3_engine import PyTTSx3TextToSpeech
