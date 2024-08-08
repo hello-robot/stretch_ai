3b33c8cc5765bc927c0709820748d019bc7f1145
Chris Paxton <cpaxton@hello-robot.com>
2024-08-08 11:52:50 -0400

    update license header in license headers file

--- scripts/fix_license_headers.py
+++ scripts/fix_license_headers.py
@@ -7,31 +7,6 @@
 # Some code may be adapted from other open-source works with their respective licenses. Original
 # license information maybe found below, if so.
 
-# Copyright (c) Hello Robot, Inc.
-# All rights reserved.
-#
-# This source code is licensed under the license found in the LICENSE file in the root directory of
-# this source tree.
-#
-# Some code may be adapted from other open-source works with their respective licenses. Original
-# license information maybe found below, if so.
-
-# Copyright (c) Hello Robot, Inc.
-# All rights reserved.
-#
-# This source code is licensed under the license found in the LICENSE file in the root directory of this source tree.
-#
-# Some code may be adapted from other open-source works with their respective licenses. Original
-# license information maybe found below, if so.
-
-# Copyright (c) Hello Robot, Inc.
-# All rights reserved.
-#
-# This source code is licensed under the license found in the LICENSE file in the root directory of this source tree.
-#
-# Some code may be adapted from other open-source works with their respective licenses. Original
-# licence information maybe found below, if so.
-
 """
 This is a tool to add license headers to all files in the repo.
 """
