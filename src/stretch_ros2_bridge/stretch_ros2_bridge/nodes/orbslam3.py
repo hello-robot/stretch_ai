82c42b0b8a2e467ad8f274289b6caa22bbafc7b4
Atharva Pusalkar <atharva@hello-robot.com>
2024-08-24 21:26:28 -0400

    Tune orb parameters and fix internal module

--- src/stretch_ros2_bridge/stretch_ros2_bridge/nodes/orbslam3.py
+++ src/stretch_ros2_bridge/stretch_ros2_bridge/nodes/orbslam3.py
@@ -128,19 +128,6 @@ class OrbSlam3(Node):
             content = None
             with open(file.name, "r") as f:
                 content = f.read()
-
-            # Add IMU extrinsics to the end of the file. These values are for Intel RealSense D435i
-            content += """\n"""
-            """# Transformation from body-frame (imu) to left camera\n"""
-            """IMU.T_b_c1: !!opencv-matrix\n"""
-            """   rows: 4\n"""
-            """   cols: 4\n"""
-            """   dt: f\n"""
-            """   data: [0.999903, -0.0138036, -0.00208099, -0.0202141,\n"""
-            """         0.0137985, 0.999902, -0.00243498, 0.00505961,\n"""
-            """         0.0021144, 0.00240603, 0.999995, 0.0114047,\n"""
-            """         0.0, 0.0, 0.0, 1.0]\n"""
-
             with open(file.name, "w") as f:
                 f.write("%YAML:1.0\n" + content)
 
