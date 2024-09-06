d399262486a708abdbdead0f388a49c3745bab32
Chris Paxton <cpaxton@hello-robot.com>
2024-09-06 14:11:10 -0400

    various updates

--- src/stretch/mapping/voxel/voxel.py
+++ src/stretch/mapping/voxel/voxel.py
@@ -554,9 +554,12 @@ class SparseVoxelMap(object):
         if self._add_local_radius_points and len(self.observations) == 0:
             # Only do this at the first step, never after it.
             # TODO: just get this from camera_pose?
-            self._update_visited(camera_pose[:3, 3].to(self.map_2d_device))
-        if base_pose is not None:
-            self._update_visited(base_pose.to(self.map_2d_device))
+            # Add local radius points to the map around base
+            if base_pose is not None:
+                self._update_visited(base_pose.to(self.map_2d_device))
+            else:
+                # Camera only
+                self._update_visited(camera_pose[:3, 3].to(self.map_2d_device))
 
         # Increment sequence counter
         self._seq += 1
