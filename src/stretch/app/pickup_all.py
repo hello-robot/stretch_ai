b11a3285915fa583a8918fc87916a9e144b3a9b0
Chris Paxton <cpaxton@hello-robot.com>
2024-09-12 14:06:57 -0400

    update example

--- src/stretch/app/pickup_all.py
+++ src/stretch/app/pickup_all.py
@@ -12,6 +12,7 @@
 import click
 
 from stretch.agent.robot_agent import RobotAgent
+from stretch.agent.task.pickup import PickupTask
 from stretch.agent.zmq_client import HomeRobotZmqClient
 from stretch.core import get_parameters
 from stretch.perception import create_semantic_sensor
@@ -26,7 +27,19 @@ from stretch.perception import create_semantic_sensor
     help="Set if we are executing on the robot and not on a remote computer",
 )
 @click.option("--parameter_file", default="default_planner.yaml", help="Path to parameter file")
-@click.option("--target_object", type=str, default="toy", help="Type of object to pick up and move")
+@click.option(
+    "--target_object",
+    type=str,
+    default="toy",
+    help="Type of object to pick up from the floor and move",
+)
+@click.option(
+    "--receptacle",
+    "--target_receptacle",
+    type=str,
+    default="box",
+    help="Type of receptacle to place the object in",
+)
 @click.option(
     "--force-rotate",
     "--force_rotate",
@@ -34,10 +47,11 @@ from stretch.perception import create_semantic_sensor
     help="Force the robot to rotate in place before doing anything.",
 )
 @click.option(
-    "--destination",
-    type=str,
-    default="box",
-    help="Where to put the objects once you have found them",
+    "--match_method",
+    type=click.Choice(["class", "feature"]),
+    default="class",
+    help="Method to match objects to pick up. Options: class, feature.",
+    show_default=True,
 )
 @click.option(
     "--mode",
@@ -54,9 +68,10 @@ def main(
     show_intermediate_maps: bool = False,
     reset: bool = False,
     target_object: str = "shoe",
-    destination: str = "box",
+    receptacle: str = "box",
     force_rotate: bool = False,
     mode: str = "one_shot",
+    match_method: str = "feature",
 ):
     """Set up the robot, create a task plan, and execute it."""
     # Create robot
@@ -67,19 +82,36 @@ def main(
         parameters=parameters,
     )
     semantic_sensor = create_semantic_sensor(
-        parameters,
+        parameters=parameters,
         device_id=device_id,
         verbose=verbose,
     )
 
+    # Start moving the robot around
+    grasp_client = None
+
     # Agents wrap the robot high level planning interface for now
-    agent = RobotAgent(robot, parameters, semantic_sensor)
+    agent = RobotAgent(robot, parameters, semantic_sensor, grasp_client=grasp_client)
     agent.start(visualize_map_at_start=show_intermediate_maps)
+    if reset:
+        agent.move_closed_loop([0, 0, 0], max_time=60.0)
+
+    # After the robot has started...
+    try:
+        pickup_task = PickupTask(
+            agent, target_object=target_object, target_receptacle=receptacle, matching=match_method
+        )
+        task = pickup_task.get_task(add_rotate=force_rotate, mode=mode)
+    except Exception as e:
+        print(f"Error creating task: {e}")
+        robot.stop()
+        raise e
+
+    task.run()
 
     if reset:
         # Send the robot home at the end!
         agent.go_home()
-        agent.move_closed_loop([0, 0, 0], max_time=60.0)
 
     # At the end, disable everything
     robot.stop()
