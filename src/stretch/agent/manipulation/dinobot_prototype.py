88cee15298dfff1a214764a8c5d832e5058c3e37
Mohamed Fazil <mfazil@hello-robot.com>
2024-09-12 00:47:47 -0700

    Add mask to dinobot

--- src/stretch/agent/manipulation/dinobot_prototype.py
+++ src/stretch/agent/manipulation/dinobot_prototype.py
@@ -12,10 +12,12 @@ import sys
 sys.path.append('/home/hello-robot/repos/dino-vit-features')
 from correspondences import find_correspondences, visualize_correspondences
 from extractor import ViTExtractor
+import matplotlib.pyplot as plt
 import torch
 import time
 import numpy as np
 from stretch.agent import RobotClient
+from stretch.perception.detection.detic import DeticPerception
 
 # image_path1 = "/home/hello-robot/ee_rgb_1.png" #@param
 # image_path2 = "/home/hello-robot/ee_rgb_0.png" #@param
@@ -41,6 +43,10 @@ model_type='dino_vits8' #@param
 #@markdown Choose stride:
 stride=4 #@param
 
+
+track_object_id = 41  # detic object id for cup
+
+
 class Dinobot:
     def __init__(self, model_type: str = 'dino_vits8', stride: int = 4):
         self.robot = RobotClient(robot_ip="10.0.0.14")
@@ -63,31 +69,46 @@ class Dinobot:
                                                                         thresh)
         return points1, points2
 
-    def run(self):
+    def run(self, visualize=False):
         print("Running Dinobot")
         while True:
             obs = self.robot.get_observation()
             servo = self.robot.get_servo_observation()
             ee_rgb = cv2.cvtColor(servo.ee_rgb, cv2.COLOR_BGR2RGB)
             ee_depth = servo.ee_depth
-
             if not isinstance(self.bottleneck_image,type(None)):
                 start = time.perf_counter()
                 with torch.no_grad():
                     points1, points2 = self.get_correspondences(self.bottleneck_image, servo.ee_rgb)
-                    if len(points1) == len(points2):
-                        im1, im2 = visualize_correspondences(points1, points2, self.bottleneck_image, servo.ee_rgb)
-                        cv2.imshow("Correspondences", cv2.cvtColor(np.hstack([im1,im2]), cv2.COLOR_BGR2RGB))
-                    else:
-                        print("No correspondences found")
                 inf_ts = (time.perf_counter() - start) * 1000
                 print(f"\n  current: {inf_ts} ms")
-            if cv2.waitKey(1) & 0xFF == ord("q"):
-                cv2.destroyAllWindows()
-                self.robot.stop()
+                if visualize:
+                    if len(points1) == len(points2):
+                        im1, im2 = visualize_correspondences(points1, points2, self.bottleneck_image, servo.ee_rgb)
+                        fig, axes = plt.subplots(1, 2, figsize=(10, 5))
+                        axes[0].imshow(im1)
+                        axes[0].set_title('Bottleneck Image')
+                        axes[0].axis('off')
+                        axes[1].imshow(im2)
+                        axes[1].set_title('Live Image')
+                        axes[1].axis('off')
+                        plt.show()
+                    else:
+                        print("No correspondences found")
+    
+    def update_bottleneck_image(self, image):
+        self.bottleneck_image = image
                 
 
 if __name__ == "__main__":
     dinobot = Dinobot()
-    dinobot.bottleneck_image = dinobot.robot.get_servo_observation().ee_rgb
-    dinobot.run()
+    detic = DeticPerception()
+
+    bottleneck_image = dinobot.robot.get_servo_observation().ee_rgb
+    semantic, instance, task_observations = detic.predict(bottleneck_image)
+    if track_object_id in task_observations["instance_classes"]:
+        object_mask = semantic == track_object_id
+        bottleneck_image[~object_mask] = [0, 0, 0]
+
+    dinobot.update_bottleneck_image(bottleneck_image)
+    dinobot.run(visualize=True)
