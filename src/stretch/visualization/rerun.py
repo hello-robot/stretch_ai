cbbe27aaa12bca7d044ca8595d8b0cc2adabf70e
Mohamed Fazil <mfazil@hello-robot.com>
2024-09-15 18:13:41 -0700

    Robot collision mesh viz init

--- src/stretch/visualization/rerun.py
+++ src/stretch/visualization/rerun.py
@@ -22,7 +22,7 @@ from stretch.mapping.scene_graph import SceneGraph
 from stretch.mapping.voxel.voxel_map import SparseVoxelMapNavigationSpace
 from stretch.motion import HelloStretchIdx
 from stretch.perception.wrapper import OvmmPerception
-from stretch.visualization import rr_robot
+from stretch.visualization import urdf_visualizer
 
 
 def decompose_homogeneous_matrix(homogeneous_matrix: np.ndarray) -> Tuple[np.ndarray, np.ndarray]:
@@ -103,7 +103,7 @@ class RerunVsualizer:
         rr.init("Stretch_robot", spawn=False)
         rr.serve(open_browser=True, server_memory_limit="2GB")
 
-        self.urdf_logger = rr_robot.StretchURDFLogger()
+        self.urdf_logger = urdf_visualizer.StretchURDFLogger()
 
         self.urdf_logger.log()
         # Create environment Box place holder
