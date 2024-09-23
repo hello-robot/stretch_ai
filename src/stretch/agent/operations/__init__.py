8f3e8e8d5698e735b694779f3b8774fbe4eb0e43
hello-atharva <134000495+hello-atharva@users.noreply.github.com>
2024-09-23 10:28:22 -0400

    Compile LLM code policies to managed operations (#117)
    * Add LLM plan compiler
    
    * pre-commit and program flow fixes
    
    * Remove task manager from ovmm
    
    * Fix compilation process
    
    * Fix compilation
    
    * Latest changes for managed operations
    
    * Final fixes
    
    * Add intermediate pregrasp
    
    * Use target_object flag
    
    * Rename TreeNode to LLMTreeNode
    
    * update to make more verbose
    
    * OVMM instructions
    
    * update this and make it print more information out
    
    * updates
    
    * Add class description
    
    * fix dumb error with cd src
    
    ---------
    
    Co-authored-by: Chris Paxton <cpaxton@hello-robot.com>

--- src/stretch/agent/operations/__init__.py
+++ src/stretch/agent/operations/__init__.py
@@ -16,11 +16,13 @@ from .emote import (
     WaveOperation,
     WithdrawOperation,
 )
+from .go_to import GoToOperation
 from .grasp_object import GraspObjectOperation
 from .navigate import NavigateToObjectOperation
 from .place_object import PlaceObjectOperation
 from .pregrasp import PreGraspObjectOperation
 from .rotate_in_place import RotateInPlaceOperation
 from .search_for_object import SearchForObjectOnFloorOperation, SearchForReceptacleOperation
+from .speak import SpeakOperation
 from .switch_mode import GoToNavOperation
 from .update import UpdateOperation
