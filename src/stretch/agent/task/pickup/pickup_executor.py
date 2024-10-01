df5e510b8a286ad57fb27314696493fc71f499f7
Chris Paxton <cpaxton@hello-robot.com>
2024-10-01 15:21:31 -0400

    add robot task executor

--- src/stretch/agent/task/pickup/pickup_executor.py
+++ src/stretch/agent/task/pickup/pickup_executor.py
@@ -23,14 +23,28 @@ class PickupExecutor:
 
     def __call__(self, response: List[Tuple[str, str]]) -> None:
         """Execute the list of commands."""
-        for i, (command, args) in enumerate(response):
+        i = 0
+
+        # Loop over every command we have been given
+        # Pull out pickup and place as a single arg if they are in a row
+        # Else, execute things as they come
+        while i < len(response):
+            command, args = response[i]
+            logger.info(f"{i} {command} {args}")
             if command == "say":
-                logger.info(f"{i} {command} {args}")
                 self.agent.robot_say(args)
             elif command == "pickup":
-                self.agent.pickup(args)
+                target_object = args
+                i += 1
+                next_command, next_args = response[i]
+                if next_command != "place":
+                    i -= 1
+                    logger.error("Pickup without place! Doing nothing.")
+                else:
+                    logger.info(f"{i} {command} {args}")
+                breakpoint()
             elif command == "place":
-                self.agent.place(args)
+                logger.error("Place without pickup! Doing nothing.")
             elif command == "wave":
                 self.agent.wave()
             elif command == "go_home":
@@ -46,5 +60,4 @@ class PickupExecutor:
             elif command == "end":
                 break
             else:
-                # logger.error(f"Unknown command: {command}")
-                pass
+                logger.error(f"Skipping unknown command: {command}")
