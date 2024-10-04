6728afc186d4caaface10e71d65da75124e13950
Chris Paxton <cpaxton@hello-robot.com>
2024-10-04 14:00:37 -0400

    update

--- src/stretch/agent/operations/place_object.py
+++ src/stretch/agent/operations/place_object.py
@@ -25,7 +25,7 @@ class PlaceObjectOperation(ManagedOperation):
     lift_distance: float = 0.2
     place_height_margin: float = 0.1
     show_place_in_voxel_grid: bool = False
-    place_step_size: float = 0.4
+    place_step_size: float = 0.35
     use_pitch_from_vertical: bool = True
     verbose: bool = True
     talk: bool = True
