23ec459a08558530cd245c0f37439765ce6f8e10
Atharva Pusalkar <atharvapusalkar18@gmail.com>
2024-10-07 10:16:18 -0400

    Add speak and pickup tasks

--- src/stretch/agent/task/pickup/pickup_executor.py
+++ src/stretch/agent/task/pickup/pickup_executor.py
@@ -9,10 +9,25 @@
 
 from typing import List, Tuple
 
+from stretch.agent.operations import (
+    GoToNavOperation,
+    GoToOperation,
+    GraspObjectOperation,
+    NavigateToObjectOperation,
+    OpenLoopGraspObjectOperation,
+    PreGraspObjectOperation,
+    PlaceObjectOperation,
+    RotateInPlaceOperation,
+    SearchForObjectOnFloorOperation,
+    SearchForReceptacleOperation,
+    SpeakOperation,
+    WaveOperation,
+)
 from stretch.agent.robot_agent import RobotAgent
 from stretch.agent.task.emote import EmoteTask
 from stretch.agent.task.pickup.pickup_task import PickupTask
 from stretch.core import AbstractRobotClient
+from stretch.core.task import Operation, Task
 from stretch.utils.logger import Logger
 
 logger = Logger(__name__)
@@ -55,6 +70,141 @@ class PickupExecutor:
         # Configuration
         self._match_method = match_method
         self._open_loop = open_loop
+
+        # Managed task
+        self._task = Task()
+        self._operation_count = 0
+
+
+    def say(self, text: str) -> None:
+        """Use the robot to say the text."""
+        say_operation = SpeakOperation(f"{str(self._operation_count)}_say_" + text, agent=self.agent, robot=self.robot)
+        say_operation.configure(message=text)
+        self._task.add_operation(say_operation, True)
+        self._operation_count += 1
+
+    def pickup(self, target_object: str, target_receptacle: str) -> None:
+        """Pick up the object and place it in the receptacle."""
+        # Put the robot into navigation mode
+        go_to_navigation_mode = GoToNavOperation(
+            f"{str(self._operation_count)}_go to navigation mode", self.agent, retry_on_failure=True
+        )
+        self._operation_count += 1
+
+        # Spin in place to find objects.
+        rotate_in_place = RotateInPlaceOperation(
+            f"{str(self._operation_count)}_rotate_in_place", self.agent, parent=go_to_navigation_mode
+        )
+        self._operation_count += 1
+
+        # Look for the target receptacle
+        search_for_receptacle = SearchForReceptacleOperation(
+            f"{str(self._operation_count)}_search_for_{self.target_receptacle}",
+            self.agent,
+            parent=rotate_in_place,
+            retry_on_failure=True,
+            match_method=self._match_method,
+        )
+        self._operation_count += 1
+
+        # Try to expand the frontier and find an object; or just wander around for a while.
+        search_for_object = SearchForObjectOnFloorOperation(
+            f"{str(self._operation_count)}_search_for_{self.target_object}_on_floor",
+            self.agent,
+            retry_on_failure=True,
+            match_method=self._match_method,
+        )
+        self._operation_count += 1
+        if self.agent.target_object is not None:
+            # Overwrite the default object to search for
+            search_for_object.set_target_object_class(self.agent.target_object)
+        if self.agent.target_receptacle is not None:
+            search_for_receptacle.set_target_object_class(self.agent.target_receptacle)
+
+        # After searching for object, we should go to an instance that we've found. If we cannot do that, keep searching.
+        go_to_object = NavigateToObjectOperation(
+            f"{str(self._operation_count)}_go_to_object",
+            self.agent,
+            parent=search_for_object,
+            on_cannot_start=search_for_object,
+            to_receptacle=False,
+        )
+        self._operation_count += 1
+
+        # After searching for object, we should go to an instance that we've found. If we cannot do that, keep searching.
+        go_to_receptacle = NavigateToObjectOperation(
+            f"{str(self._operation_count)}_go_to_receptacle",
+            self.agent,
+            on_cannot_start=search_for_receptacle,
+            to_receptacle=True,
+        )
+        self._operation_count += 1
+
+        # When about to start, run object detection and try to find the object. If not in front of us, explore again.
+        # If we cannot find the object, we should go back to the search_for_object operation.
+        # To determine if we can start, we just check to see if there's a detectable object nearby.
+        pregrasp_object = PreGraspObjectOperation(
+            f"{str(self._operation_count)}_prepare_to_grasp",
+            self.agent,
+            on_failure=None,
+            on_cannot_start=go_to_object,
+            retry_on_failure=True,
+        )
+        self._operation_count += 1
+        # If we cannot start, we should go back to the search_for_object operation.
+        # To determine if we can start, we look at the end effector camera and see if there's anything detectable.
+        if self.use_visual_servoing_for_grasp:
+            grasp_object = GraspObjectOperation(
+                f"{str(self._operation_count)}_grasp_the_{self.target_object}",
+                self.agent,
+                parent=pregrasp_object,
+                on_failure=pregrasp_object,
+                on_cannot_start=go_to_object,
+                retry_on_failure=False,
+            )
+            grasp_object.set_target_object_class(self.agent.target_object)
+            grasp_object.servo_to_grasp = True
+            grasp_object.match_method = self._match_method
+            self._operation_count += 1
+        else:
+            grasp_object = OpenLoopGraspObjectOperation(
+                f"{str(self._operation_count)}_grasp_the_{self.target_object}",
+                self.agent,
+                parent=pregrasp_object,
+                on_failure=pregrasp_object,
+                on_cannot_start=go_to_object,
+                retry_on_failure=False,
+            )
+            grasp_object.set_target_object_class(self.agent.target_object)
+            grasp_object.match_method = self._match_method
+            self._operation_count += 1
+
+        place_object_on_receptacle = PlaceObjectOperation(
+            f"{str(self._operation_count)}_place_object_on_receptacle", self.agent, on_cannot_start=go_to_receptacle
+        )
+        self._operation_count += 1
+
+        self._task.add_operation(go_to_navigation_mode)
+        self._task.add_operation(rotate_in_place)
+        self._task.add_operation(search_for_receptacle)
+        self._task.add_operation(search_for_object)
+        self._task.add_operation(go_to_object)
+        self._task.add_operation(pregrasp_object)
+        self._task.add_operation(grasp_object)
+        self._task.add_operation(go_to_receptacle)
+        self._task.add_operation(place_object_on_receptacle)
+
+        self._task.connect_on_success(go_to_navigation_mode.name, search_for_receptacle.name)
+        self._task.connect_on_success(search_for_receptacle.name, search_for_object.name)
+        self._task.connect_on_success(search_for_object.name, go_to_object.name)
+        self._task.connect_on_success(go_to_object.name, pregrasp_object.name)
+        self._task.connect_on_success(pregrasp_object.name, grasp_object.name)
+        self._task.connect_on_success(grasp_object.name, go_to_receptacle.name)
+        self._task.connect_on_success(go_to_receptacle.name, place_object_on_receptacle.name)
+
+        self._task.connect_on_success(search_for_receptacle.name, search_for_object.name)
+
+        self._task.connect_on_cannot_start(go_to_object.name, search_for_object.name)
 
     def _pickup(self, target_object: str, target_receptacle: str) -> None:
 
