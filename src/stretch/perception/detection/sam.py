c247451a85574aac3d80c4e9e4f7057ee751fce8
Chris Paxton <cpaxton@hello-robot.com>
2024-10-18 14:53:03 -0400

    segment anything codebase

--- src/stretch/perception/detection/sam.py
+++ src/stretch/perception/detection/sam.py
@@ -15,6 +15,9 @@ import torch
 from segment_anything import SamPredictor, sam_model_registry
 
 from stretch.core.abstract_perception import PerceptionModule
+from stretch.utils.logger import Logger
+
+logger = Logger(__name__)
 
 
 class SAMPerception(PerceptionModule):
@@ -35,11 +38,11 @@ class SAMPerception(PerceptionModule):
     def _download_checkpoint_if_needed(self):
         """Try to download the checkpoint if it doesn't exist."""
         if not os.path.exists(self.checkpoint_name):
-            print(f"Downloading {self.checkpoint_name}...")
+            logger.alert(f"Downloading {self.checkpoint_name}...")
             torch.hub.download_url_to_file(self.checkpoint_url, self.checkpoint_name)
-            print("Download complete.")
+            logger.alert("Download complete.")
         else:
-            print(f"Checkpoint {self.checkpoint_name} already exists.")
+            logger.info(f"Checkpoint {self.checkpoint_name} already exists.")
 
     def _initialize_predictor(self):
         """Initialize the SAM model and predictor."""
@@ -71,9 +74,21 @@ class SAMPerception(PerceptionModule):
             print("SAM is segmenting the image...")
 
         masks, scores, logits = self.segment(rgb)
-        breakpoint()
 
         height, width, _ = rgb.shape
+        semantic = np.zeros((height, width), dtype=np.uint8)
+        instance = np.zeros((height, width), dtype=np.uint8)
+
+        for i, mask in enumerate(masks):
+            semantic[mask > 0] = i + 1
+            instance[mask > 0] = i + 1
+
+        task_observations = dict()
+        task_observations["instance_scores"] = scores
+        task_observations["instance_logits"] = logits
+        task_observations["semantic_frame"] = semantic
+
+        return semantic, instance, task_observations
 
     def segment(self, rgb, point_coords=None, point_labels=None, box=None, multimask_output=True):
         """Segment the image using loaded Segment Anything Model.
@@ -104,7 +119,10 @@ class SAMPerception(PerceptionModule):
                 box=box, multimask_output=multimask_output
             )
         else:
-            raise ValueError("Either point_coords and point_labels, or box must be provided")
+            # Box over the whole image
+            masks, scores, logits = self.predictor.predict(
+                box=np.array([0, 0, rgb.shape[1], rgb.shape[0]]), multimask_output=multimask_output
+            )
 
         return masks, scores, logits
 
