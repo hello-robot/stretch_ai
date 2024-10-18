8761511bdcb4a4727b94d118bc63572aaabe3222
Chris Paxton <cpaxton@hello-robot.com>
2024-10-18 15:04:56 -0400

    update sam code

--- src/stretch/perception/detection/sam.py
+++ src/stretch/perception/detection/sam.py
@@ -29,11 +29,16 @@ checkpoint_names = {
 
 class SAMPerception(PerceptionModule):
     def __init__(
-        self, model_type="vit_b", checkpoint_name: Optional[str] = None, verbose: bool = True
+        self,
+        model_type="vit_l",
+        checkpoint_name: Optional[str] = None,
+        verbose: bool = True,
+        default_spacing: int = 32,
     ):
         super().__init__()
         self._verbose = verbose
         self.model_type = model_type
+        self.default_spacing = default_spacing
         if checkpoint_name is None:
             if model_type not in checkpoint_names:
                 raise ValueError(
@@ -134,8 +139,11 @@ class SAMPerception(PerceptionModule):
             )
         else:
             # Box over the whole image
+            point_coords, point_labels = generate_grid_points(rgb.shape[:2], self.default_spacing)
             masks, scores, logits = self.predictor.predict(
-                box=np.array([0, 0, rgb.shape[1], rgb.shape[0]]), multimask_output=multimask_output
+                point_coords=point_coords,
+                point_labels=point_labels,
+                multimask_output=multimask_output,
             )
         t1 = timeit.default_timer()
         if self._verbose:
@@ -150,3 +158,33 @@ class SAMPerception(PerceptionModule):
     def is_instance(self):
         """Whether the perception model is an instance segmentation model."""
         return True
+
+
+def generate_grid_points(image_shape, grid_spacing):
+    """
+    Generate a grid of points and labels for an image.
+
+    Args:
+    image_shape (tuple): Shape of the image (height, width).
+    grid_spacing (int): Spacing between grid points.
+
+    Returns:
+    tuple: (point_coords, point_labels)
+        point_coords: numpy array of shape (N, 2) containing x, y coordinates
+        point_labels: numpy array of shape (N,) containing labels (all set to 1)
+    """
+    height, width = image_shape[:2]
+
+    # Generate grid points
+    x_coords = np.arange(grid_spacing // 2, width, grid_spacing)
+    y_coords = np.arange(grid_spacing // 2, height, grid_spacing)
+
+    xx, yy = np.meshgrid(x_coords, y_coords)
+
+    # Reshape to (N, 2) array
+    point_coords = np.column_stack((xx.ravel(), yy.ravel()))
+
+    # Generate labels (all positive)
+    point_labels = np.ones(point_coords.shape[0], dtype=int)
+
+    return point_coords, point_labels
