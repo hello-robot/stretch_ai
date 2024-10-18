86c357d15ee27636d002d05ecccdfa4576754da3
Chris Paxton <cpaxton@hello-robot.com>
2024-10-18 11:12:19 -0400

    update version information

--- scripts/run_stretch_ai_ros2_bridge_server.sh
+++ scripts/run_stretch_ai_ros2_bridge_server.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 echo "Starting Stretch AI ROS2 Bridge Server on $HELLO_FLEET_ID"
 echo "========================================================="
+VERSION=`python src/stretch/version.py`
+echo "Running docker image hellorobotinc/stretch-ai-ros2-bridge:$VERSION"
 sudo docker run -it \
     --net=host \
     --privileged \
@@ -8,5 +10,5 @@ sudo docker run -it \
     -v /home/hello-robot/stretch_user:/home/hello-robot/stretch_user \
     -v /home/hello-robot/ament_ws/install/stretch_description/share/stretch_description/urdf:/home/hello-robot/stretch_description/share/stretch_description/urdf \
     -e HELLO_FLEET_ID=$HELLO_FLEET_ID \
-    hellorobotinc/stretch-ai-ros2-bridge \
+    hellorobotinc/stretch-ai-ros2-bridge:$VERSION \
     bash -c "source /home/hello-robot/.bashrc; export HELLO_FLEET_ID=$HELLO_FLEET_ID; ros2 launch stretch_ros2_bridge server.launch.py"
