f5bfc79efabf627c60a919d7d8cfd4011a0a8345
Chris Paxton <cpaxton@hello-robot.com>
2024-10-23 08:06:08 -0400

    update docker

--- scripts/run_stretch_ai_ros2_bridge_server.sh
+++ scripts/run_stretch_ai_ros2_bridge_server.sh
@@ -17,8 +17,8 @@ sudo docker run -it \
     --net=host \
     --privileged \
     -v /dev:/dev \
-    -v /home/hello-robot/stretch_user:/home/hello-robot/stretch_user \
-    -v /home/hello-robot/ament_ws/install/stretch_description/share/stretch_description/urdf:/home/hello-robot/stretch_description/share/stretch_description/urdf \
+    -v /home/$USER/stretch_user:/home/hello-robot/stretch_user \
+    -v /home/$USER/ament_ws/install/stretch_description/share/stretch_description/urdf:/home/hello-robot/stretch_description/share/stretch_description/urdf \
     -e HELLO_FLEET_ID=$HELLO_FLEET_ID \
     hellorobotinc/stretch-ai-ros2-bridge:$VERSION \
     bash -c "source /home/hello-robot/.bashrc; export HELLO_FLEET_ID=$HELLO_FLEET_ID; ros2 launch stretch_ros2_bridge server.launch.py"
