7d52237df404df59b092e56abdc8290c44b8ef03
Chris Paxton <cpaxton@hello-robot.com>
2024-10-23 08:40:46 -0400

    setup script to copy contents; logs are not saved

--- scripts/run_stretch_ai_ros2_bridge_server.sh
+++ scripts/run_stretch_ai_ros2_bridge_server.sh
@@ -17,8 +17,8 @@ sudo docker run -it \
     --net=host \
     --privileged \
     -v /dev:/dev \
-    -v /home/$USER/stretch_user:/home/hello-robot/stretch_user \
+    -v /home/$USER/stretch_user:/home/hello-robot/stretch_user_copy \
     -v /home/$USER/ament_ws/install/stretch_description/share/stretch_description/urdf:/home/hello-robot/stretch_description/share/stretch_description/urdf \
     -e HELLO_FLEET_ID=$HELLO_FLEET_ID \
     hellorobotinc/stretch-ai-ros2-bridge:$VERSION \
-    bash -c "source /home/hello-robot/.bashrc; export HELLO_FLEET_ID=$HELLO_FLEET_ID; ros2 launch stretch_ros2_bridge server.launch.py"
+    bash -c "source /home/hello-robot/.bashrc; cp -r /home/hello-robot/stretch_user_copy/* /home/hello-robot/stretch_user/*; export HELLO_FLEET_ID=$HELLO_FLEET_ID; ros2 launch stretch_ros2_bridge server.launch.py"
