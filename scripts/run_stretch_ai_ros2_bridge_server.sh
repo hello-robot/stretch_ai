d625449dfdecc62bff603feeacedda7d0b0d11d1
Chris Paxton <cpaxton@hello-robot.com>
2024-10-23 08:39:17 -0400

    update files

--- scripts/run_stretch_ai_ros2_bridge_server.sh
+++ scripts/run_stretch_ai_ros2_bridge_server.sh
@@ -12,13 +12,16 @@ parent_dir="$(dirname "$script_dir")"
 echo "Reading version from $parent_dir/src/stretch/version.py"
 VERSION=`python3 $parent_dir/src/stretch/version.py`
 
+# sudo chown -R $USER:$USER /home/$USER/stretch_user
+# sudo chown -R $USER:$USER /home/$USER/ament_ws/install/stretch_description/share/stretch_description/urdf
+
 echo "Running docker image hellorobotinc/stretch-ai-ros2-bridge:$VERSION"
 sudo docker run -it \
     --net=host \
     --privileged \
     -v /dev:/dev \
-    -v /home/hello-robot/stretch_user:/home/hello-robot/stretch_user \
+    -v /home/hello-robot/stretch_user:/home/hello-robot/stretch_user_copy \
     -v /home/hello-robot/ament_ws/install/stretch_description/share/stretch_description/urdf:/home/hello-robot/stretch_description/share/stretch_description/urdf \
     -e HELLO_FLEET_ID=$HELLO_FLEET_ID \
     hellorobotinc/stretch-ai-ros2-bridge:$VERSION \
-    bash -c "source /home/hello-robot/.bashrc; export HELLO_FLEET_ID=$HELLO_FLEET_ID; ros2 launch stretch_ros2_bridge server.launch.py"
+    bash -c "source /home/hello-robot/.bashrc; cp -r /home/hello-robot/stretch_user_copy/* /home/hello-robot/stretch_user/*; export HELLO_FLEET_ID=$HELLO_FLEET_ID; ros2 launch stretch_ros2_bridge server.launch.py"
