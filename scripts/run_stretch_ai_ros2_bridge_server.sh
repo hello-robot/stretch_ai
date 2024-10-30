39c3a2f4e97522ccf48ea6d335bba759a436b90d
Chris Paxton <165678659+hello-cpaxton@users.noreply.github.com>
2024-10-30 12:43:34 -0400

    Update docker scripts to use latest version of image (#163)
    * update find task
    
    * add a printout in the GPU docker build script
    
    * update all the scripts to just use latest
    
    * update docker so we do not always need sudo
    
    * check to see if in docker group first
    
    * Add some more instructions for docker

--- scripts/run_stretch_ai_ros2_bridge_server.sh
+++ scripts/run_stretch_ai_ros2_bridge_server.sh
@@ -4,20 +4,43 @@
 # Make sure it fails if we see any errors
 set -e
 
+# Function to check if user is in Docker group
+is_in_docker_group() {
+    groups | grep -q docker
+}
+
+# Function to run Docker command
+run_docker_command() {
+    if is_in_docker_group; then
+        echo "User is in Docker group. Running command without sudo."
+        docker "$@"
+    else
+        echo "User is not in Docker group. Running command with sudo."
+        echo "To run without sudo, add your user to the docker group: sudo usermod -aG docker $USER"
+        echo "Then log out and log back in."
+        echo "Alternately, you can change for the current shell with newgrp: newgrp docker"
+        sudo docker "$@"
+    fi
+}
+
 echo "Starting Stretch AI ROS2 Bridge Server on $HELLO_FLEET_ID"
 echo "========================================================="
 script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 parent_dir="$(dirname "$script_dir")"
 
-echo "Reading version from $parent_dir/src/stretch/version.py"
+# echo "Reading version from $parent_dir/src/stretch/version.py"
 VERSION=`python3 $parent_dir/src/stretch/version.py`
+echo "Source version: $VERSION"
+
+VERSION="latest"
+echo "Docker image version: $VERSION"
 
 # sudo chown -R $USER:$USER /home/$USER/stretch_user
 # sudo chown -R $USER:$USER /home/$USER/ament_ws/install/stretch_description/share/stretch_description/urdf
 
 echo "Running docker image hellorobotinc/stretch-ai-ros2-bridge:$VERSION"
 # Make sure the image is up to date
-sudo docker pull hellorobotinc/stretch-ai-ros2-bridge:$VERSION
+run_docker_command pull hellorobotinc/stretch-ai-ros2-bridge:$VERSION
 # Run the container
 # The options mean:
 # --net=host: use host network, so that the container can communicate with the robot
@@ -34,7 +57,7 @@ sudo docker pull hellorobotinc/stretch-ai-ros2-bridge:$VERSION
 # -e HELLO_FLEET_ID=$HELLO_FLEET_ID: set the fleet ID
 # hellorobotinc/stretch-ai-ros2-bridge:$VERSION: the docker image to run
 # bash -c "source /home/hello-robot/.bashrc; cp -rf /home/hello-robot/stretch_user_copy/* /home/hello-robot/stretch_user; export HELLO_FLEET_ID=$HELLO_FLEET_ID; ros2 launch stretch_ros2_bridge server_no_d405.launch.py": run the server
-sudo docker run -it --rm \
+run_docker_command run -it --rm \
     --net=host \
     --privileged=true \
     -v /dev:/dev \
