25d41c8af739b9b53dda7279fdc25615cef33b53
hello-peiqi <peiqi@hello-robot.com>
2024-11-11 10:07:39 -0500

    corl update

--- src/stretch/dynav/mapping_utils/voxel_map.py
+++ src/stretch/dynav/mapping_utils/voxel_map.py
@@ -107,7 +107,7 @@ class SparseVoxelMapNavigationSpace(XYT):
             orientation_resolution: number of bins to break it into
         """
         self._footprint = Footprint(
-            width=0.34 * 0.8, length=0.33 * 0.8, width_offset=0.0, length_offset=-0.1
+            width=0.34 * 0.7, length=0.33 * 0.7, width_offset=0.0, length_offset=-0.1
         )
         self._orientation_resolution = 64
         self._oriented_masks = []
