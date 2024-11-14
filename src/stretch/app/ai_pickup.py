943ad902bac084491fc6c738be237976d1df5263
Chris Paxton <cpaxton@hello-robot.com>
2024-11-14 12:54:12 -0500

    Add a flag to enable realtime updates but leave it off by default for now

--- src/stretch/app/ai_pickup.py
+++ src/stretch/app/ai_pickup.py
@@ -48,6 +48,12 @@ logger = Logger(__name__)
     type=click.Choice(get_llm_choices()),
 )
 @click.option(
+    "--realtime",
+    "--real-time",
+    is_flag=True,
+    help="Set to enable real-time updates",
+)
+@click.option(
     "--device_id",
     default=0,
     help="ID of the device to use for perception",
@@ -126,6 +132,7 @@ def main(
     use_voice: bool = False,
     open_loop: bool = False,
     debug_llm: bool = False,
+    realtime: bool = False,
     radius: float = 3.0,
     input_path: str = "",
 ):
@@ -151,7 +158,7 @@ def main(
         use_voice = False
 
     # Agents wrap the robot high level planning interface for now
-    agent = RobotAgent(robot, parameters, semantic_sensor, enable_realtime_updates=True)
+    agent = RobotAgent(robot, parameters, semantic_sensor, enable_realtime_updates=realtime)
     agent.start(visualize_map_at_start=show_intermediate_maps)
     if reset:
         agent.move_closed_loop([0, 0, 0], max_time=60.0)
