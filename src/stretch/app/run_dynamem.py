0d14f9a0884dcfd2253ee1c34f7d9b6f630f3fcc
Chris Paxton <cpaxton@hello-robot.com>
2024-11-25 11:31:47 -0500

    update dynamem

--- src/stretch/app/run_dynamem.py
+++ src/stretch/app/run_dynamem.py
@@ -20,20 +20,6 @@ from stretch.core.parameters import get_parameters
 from stretch.perception import create_semantic_sensor
 
 
-def compute_tilt(camera_xyz, target_xyz):
-    """
-    a util function for computing robot head tilts so the robot can look at the target object after navigation
-    - camera_xyz: estimated (x, y, z) coordinates of camera
-    - target_xyz: estimated (x, y, z) coordinates of the target object
-    """
-    if not isinstance(camera_xyz, np.ndarray):
-        camera_xyz = np.array(camera_xyz)
-    if not isinstance(target_xyz, np.ndarray):
-        target_xyz = np.array(target_xyz)
-    vector = camera_xyz - target_xyz
-    return -np.arctan2(vector[2], np.linalg.norm(vector[:2]))
-
-
 def get_mode(mode: str) -> str:
 
     if mode == "navigation":
