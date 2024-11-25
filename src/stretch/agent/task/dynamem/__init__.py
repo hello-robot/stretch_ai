3aa5ebdde35ed521f2128fb8d25b5bc333812961
Chris Paxton <cpaxton@hello-robot.com>
2024-11-25 14:14:43 -0500

    update

--- src/stretch/agent/task/dynamem/__init__.py
+++ src/stretch/agent/task/dynamem/__init__.py
@@ -6,3 +6,4 @@
 #
 # Some code may be adapted from other open-source works with their respective licenses. Original
 # license information maybe found below, if so.
+from .dynamem_task import DynamemTaskExecutor
