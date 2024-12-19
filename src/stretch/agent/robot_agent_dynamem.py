f70d234b8988c78d04773f5c83805aed8b6ab5a6
Chris Paxton <cpaxton@hello-robot.com>
2024-12-19 17:01:14 -0500

    updates to configuration

--- src/stretch/agent/robot_agent_dynamem.py
+++ src/stretch/agent/robot_agent_dynamem.py
@@ -154,9 +154,7 @@ class RobotAgent(RobotAgentBase):
         # ==============================================
 
         # Parameters for feature matching and exploration
-        self._is_match_threshold = parameters["instance_memory"]["matching"][
-            "feature_match_threshold"
-        ]
+        self._is_match_threshold = parameters.get("encoder_args/feature_match_threshold", 0.05)
 
         # Expanding frontier - how close to frontier are we allowed to go?
         self._default_expand_frontier_size = parameters["motion_planner"]["frontier"][
