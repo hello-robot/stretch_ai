54ddaf71690e3233e44a28b7d61b4242e32df90d
Peiqi Liu <peiqi@hello-robot.com>
2025-06-10 00:57:47 -0700

    update hyperparameter

--- src/stretch/agent/operations/grasp_object.py
+++ src/stretch/agent/operations/grasp_object.py
@@ -87,7 +87,7 @@ class GraspObjectOperation(ManagedOperation):
     # ------------------------
     # Grasping motion planning parameters and offsets
     # This is the distance at which we close the gripper when visual servoing
-    median_distance_when_grasping: float = 0.16
+    median_distance_when_grasping: float = 0.165
     lift_min_height: float = 0.1
     lift_max_height: float = 1.0
 
