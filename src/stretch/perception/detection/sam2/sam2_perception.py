9a4f92d10492a81a0db23d5bf241451502f28a18
Peiqi Liu <peiqi@hello-robot.com>
2025-07-04 18:55:30 -0700

    remove mobile sam

--- src/stretch/perception/detection/sam2/sam2_perception.py
+++ src/stretch/perception/detection/sam2/sam2_perception.py
@@ -8,10 +8,8 @@
 # license information maybe found below, if so.
 
 import os
-from pathlib import Path
 from typing import Any, Dict, List, Optional, Tuple
 
-import matplotlib.pyplot as plt
 import numpy as np
 import torch
 import wget
@@ -22,41 +20,6 @@ from sam2.sam2_image_predictor import SAM2ImagePredictor
 from stretch.core.abstract_perception import PerceptionModule
 from stretch.perception.detection.utils import filter_depth, overlay_masks
 
-PARENT_DIR = Path(__file__).resolve().parent
-# GroundingDINO config and checkpoint
-GROUNDING_DINO_CONFIG_PATH = str(
-    PARENT_DIR
-    / "Grounded-Segment-Anything/GroundingDINO/groundingdino/config/GroundingDINO_SwinT_OGC.py"
-)
-GROUNDING_DINO_CHECKPOINT_PATH = str(PARENT_DIR / "checkpoints" / "groundingdino_swint_ogc.pth")
-MOBILE_SAM_CHECKPOINT_PATH = str(PARENT_DIR / "checkpoints" / "mobile_sam.pt")
-BOX_THRESHOLD = 0.25
-TEXT_THRESHOLD = 0.25
-NMS_THRESHOLD = 0.8
-
-_DEFAULT_MASK_GENERATOR_KWARGS = dict(
-    points_per_side=32,
-    pred_iou_thresh=0.92,
-    crop_n_layers=1,
-    crop_n_points_downscale_factor=2,
-)
-
-
-def show_anns(anns):
-    if len(anns) == 0:
-        return
-    sorted_anns = anns
-    ax = plt.gca()
-    ax.set_autoscale_on(False)
-
-    img = np.ones((sorted_anns[0].shape[0], sorted_anns[0].shape[1], 4))
-    img[:, :, 3] = 0
-    for ann in sorted_anns:
-        m = ann
-        color_mask = np.concatenate([np.random.random(3), [0.35]])
-        img[m] = color_mask
-    ax.imshow(img)
-
 
 class SAM2Perception(PerceptionModule):
     def __init__(
@@ -64,20 +27,15 @@ class SAM2Perception(PerceptionModule):
         custom_vocabulary: List[str] = "['', 'dog', 'grass', 'sky']",
         gpu_device_id: Optional[int] = None,
         configuration: str = "l",
-        checkpoint_file: str = MOBILE_SAM_CHECKPOINT_PATH,
         verbose=False,
-        text_threshold: float = None,
-        mask_generator_kwargs: Dict[str, Any] = _DEFAULT_MASK_GENERATOR_KWARGS,
     ):
         """Load trained SAM2 model for inference.
 
         Arguments:
-            config_file: path to model config
             custom_vocabulary: if vocabulary="custom", this should be a comma-separated
              list of classes (as a single string)
             gpu_device_id: GPU ID to load the model on, None for default
             configuration: size of SAM2 model, default is t, also supporting s, b+, l
-            checkpoint_file: path to model checkpoint
             verbose: whether to print out debug information
         """
         # TODO: set model arg in config
@@ -190,20 +148,6 @@ class SAM2Perception(PerceptionModule):
             image = image.astype(np.uint8)
 
         masks = np.array(self.generate(image))
-
-        # from PIL import Image
-        # for mask in masks:
-        #     mm = np.expand_dims(mask, axis=-1)
-        #     image_array = np.array(mm * image, dtype=np.uint8)
-        #     image_debug = Image.fromarray(image_array)
-        #     image_debug.show()
-
-        # plt.figure(figsize=(20, 20))
-        # plt.imshow(image)
-        # plt.savefig("original.png", dpi=100)
-        # show_anns(masks)
-        # plt.axis("off")
-        # plt.savefig("segmented.png", dpi=100)
 
         masks = sorted(masks, key=lambda x: np.count_nonzero(x), reverse=True)
         if depth_threshold is not None and depth is not None:
